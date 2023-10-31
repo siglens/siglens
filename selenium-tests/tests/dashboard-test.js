7c53e91596fd8432c402521a034cd845a3f12e68
Zhifei-Li <113303904+Zhifei-Li@users.noreply.github.com>
2023-10-31 11:48:20 -0400

    add tests

---
+++ selenium-tests/tests/dashboard-test.js
@@ -0,0 +1,175 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+const {By,Key,Builder, until, WebDriverWait} = require("selenium-webdriver");
+const assert = require("assert");
+const chrome = require('selenium-webdriver/chrome');
+let driver;
+const chromeOptions = new chrome.Options()
+
+// chromeOptions.addArguments('--headless');
+
+async function testDashboardPageButtons(){
+    try{
+        //To wait for browser to build and launch properly
+        driver = await new Builder().forBrowser("chrome")
+                                    .setChromeOptions(chromeOptions)
+                                    .build();
+        
+        //To fetch http://localhost/dashboards-home.html from the browser with our code.
+        await driver.get("http://localhost/dashboards-home.html");
+        let searchButton = await driver.findElement(By.id("run-search"));
+        let btnTxt = await searchButton.getText();
+        assert.equal(btnTxt, "Search", 'button text is not "Search"');
+
+        let newDbBtn = await driver.findElement(By.id("create-db-btn"));
+        let newDbTxt = await newDbBtn.getText();
+        assert.equal(newDbTxt, "New Dashboard", 'button text is not "Create New Database"');
+
+        let dashboardGrid = await driver.findElement(By.id("dashboard-grid"));  
+        // check if dashboard grid is displayed
+        let dashboardGridPresent = await dashboardGrid.isDisplayed();
+        assert.equal(dashboardGridPresent, true, 'dashboard-grid is not displayed');
+    
+        // check if the Add dashboard button is clickable
+        await newDbBtn.click();
+        let addDashboardModal = await driver.findElement(By.id("new-dashboard-modal"));
+        let addDashboardModalPresent = await addDashboardModal.isDisplayed();
+        assert.equal(addDashboardModalPresent, true, 'new-dashboard-modal is not displayed');
+
+        // check if Dashboard is created
+        let dbName = "Test Dashboard";
+        let dbNameInput = await driver.findElement(By.id("db-name"));
+        let dbDescInput = await driver.findElement(By.id("db-description"));
+        let saveDbBtn = await driver.findElement(By.id("save-dbbtn"));
+        // todo check if cancel button works
+        let cancelDbBtn = await driver.findElement(By.id("cancel-dbbtn"));
+        const initialUrl = 'http://localhost/dashboards-home.html';
+
+        await cancelDbBtn.click();
+        await driver.sleep(1000);
+        let currentUrl = await driver.getCurrentUrl();
+        assert.equal(currentUrl, initialUrl, 'cancel button does not work');
+
+        await newDbBtn.click();
+        await dbNameInput.sendKeys(dbName);
+        await dbDescInput.sendKeys("Test Description");
+        await saveDbBtn.click();
+
+        await driver.wait(until.urlContains('dashboard.html'), 5000);
+        currentUrl = await driver.getCurrentUrl();
+        assert(currentUrl.includes("dashboard.html"), `URL ${currentUrl} does not contain "dashboard.html"`);
+
+        await driver.sleep(1000);
+
+        // Now check buttons on launch-dashboard.html
+       // test add panel button
+        let addPanelBtn = await driver.findElement(By.id("add-panel-btn"));
+        let addPanelTxt = await addPanelBtn.getText();
+        assert.equal(addPanelTxt, "Add Panel", 'button text is not "Add Panel"');
+
+        // test save db button
+        let saveDBBtn = await driver.findElement(By.id("save-db-btn"));
+        const saveDbImage = await saveDBBtn.findElement(By.css("img"));
+        let saveIconClassAttr = await saveDbImage.getAttribute("src");
+        assert(saveIconClassAttr.endsWith('/save-icon.svg'), 'Image icon does not point to expected file');
+
+        // test settings button
+        let settingsBtn = await driver.findElement(By.className("settings-btn"));
+        const settingsImage = await settingsBtn.findElement(By.css("img"));
+        let settingsIconClassAttr = await settingsImage.getAttribute("src");
+        assert(settingsIconClassAttr.endsWith('/settings-icon.svg'), 'Image icon does not point to expected file');
+
+        // test refresh button
+        let refreshBtn = await driver.findElement(By.className("refresh-btn"));
+        const refreshImage = await refreshBtn.findElement(By.css("img"));
+        let refreshIconClassAttr = await refreshImage.getAttribute("src");
+        assert(refreshIconClassAttr.endsWith('/refresh-icon.svg'), 'Image icon does not point to expected file');
+
+        // test time picker button
+        let timePickerBtn = await driver.findElement(By.id("date-picker-btn"));
+        let datePickerTxt = await timePickerBtn.getText();
+        assert.equal(datePickerTxt, "Time Picker", 'button text is not "Time Picker"');
+
+        //todo check if add panel btn works
+        addPanelBtn = await driver.findElement(By.id("add-panel-btn"));
+        await addPanelBtn.click();
+        
+        let panel = driver.findElement(By.className("panel"))
+        
+        let panelPresent = await panel.isDisplayed();
+        assert.equal(panelPresent, true, 'panel is not displayed');
+
+        // check panel options
+        let panelOptionsBtn = await driver.findElement(By.id("panel-options-btn"));
+        await panelOptionsBtn.click();
+        let panelOptionsModal = await driver.findElement(By.id("panel-dropdown-modal"));
+        let panelOptionsModalPresent = await panelOptionsModal.isDisplayed();
+        assert.equal(panelOptionsModalPresent, true, 'panel-dropdown-modal is not displayed');
+
+
+        // Find the dropdown list elements
+        const list = await driver.findElement(By.css('ul.dropdown-style'));
+
+        // Find all the child elements of the list
+        const items = await list.findElements(By.tagName('li'));
+
+        // Loop through the items and log their text
+        let expectedOptionList = ["View", "Edit", "Duplicate", "Remove"];
+        let actualOptionList = [];
+        for (const item of items) {
+            const text = await item.getText();
+            actualOptionList.push(text);
+        }
+        assert.deepEqual(actualOptionList, expectedOptionList, 'panel options are not as expected');
+
+        // click save db button
+        await saveDBBtn.click();
+
+        let saveDbModal = await driver.findElement(By.id("save-db-modal"));
+        let saveDbModalPresent = await saveDbModal.isDisplayed();
+        assert.equal(saveDbModalPresent, true, 'save-db-modal is not displayed');
+        let saveDbModalTxt = await saveDbModal.getText();
+        assert.equal(saveDbModalTxt, "Dashboard Updated Successfully ✖", 'modal text is not "Dashboard Updated Successfully "');
+        
+        // check if the dashboard is saved
+        let allDashboardsText = await driver.findElement(By.id("all-dashboards-text"));
+    
+        await allDashboardsText.click();
+
+        // check if you are redirected to launch-dashboard.html
+        await driver.wait(until.urlContains('dashboards-home.html'), 5000);
+        const currentUrl2 = await driver.getCurrentUrl();
+    
+        assert.equal(currentUrl2, initialUrl, 'not redirected to dashboards-home.html');
+
+        // check if the dashboard is saved
+        const aggridRows = await driver.findElements(By.css('.ag-row'));
+        assert(aggridRows.length > 1, 'No rows found in the ag-Grid');
+
+        console.log("All dashboard tests passed")
+    }catch (err) {
+        // Handle any errors that occur during test execution
+        console.error(err);
+    
+    } finally {
+        // Close the driver instance, even if an error occurred
+        await driver.quit();
+    }
+    
+}
+
+testDashboardPageButtons();
---
+++ selenium-tests/tests/index-test.js
@@ -0,0 +1,134 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+const {By,Key,Builder} = require("selenium-webdriver");
+const assert = require("assert");
+const chrome = require('selenium-webdriver/chrome');
+let driver;
+const chromeOptions = new chrome.Options()
+
+chromeOptions.addArguments('--headless');
+
+async function testIndexPageButtons(){
+    try{
+        //To wait for browser to build and launch properly
+        driver = await new Builder().forBrowser("chrome")
+                                    .setChromeOptions(chromeOptions)
+                                    .build();
+        
+        //To fetch http://localhost/index.html from the browser with our code.
+        
+        await driver.get("http://localhost/index.html");
+            
+        // To find search buttons on the index.html page
+        
+        let indexButton = await driver.findElement(By.id("index-btn"));
+        let indexTxt = await indexButton.getText();
+        let datePickerBtn = await driver.findElement(By.id("date-picker-btn"));
+        let datePickerTxt = await datePickerBtn.getText();
+        // let saveQueryBtn = await driver.findElement(By.id("saveq-btn"));
+        // let saveQueryTxt = await saveQueryBtn.getText();
+        // let availableFieldBtn = await driver.findElement(By.id("avail-fields-btn"));
+        // let availableFieldTxt = await availableFieldBtn.getText();
+        let searchButton = await driver.findElement(By.id("run-filter-btn"));
+        let btnTxt = await searchButton.getText();
+        
+        assert.equal(btnTxt, "Search", 'button text is not "Search"');
+        assert.equal(indexTxt, "Index", 'button text is not "Index"');
+        assert.equal(datePickerTxt, "Last 15 Mins", 'button text is not "Date Picker"');
+        // assert.equal(saveQueryTxt, "Save Query", 'button text is not "Save Query"');
+        // assert.equal(availableFieldTxt, "  Available Fields", 'button text is not "Available Fields"');
+
+        // check if the dropdowns are displayed
+        let dropdownPresent;
+        await indexButton.click();
+        dropdownPresent = await driver.findElement(By.id("available-indexes")).isDisplayed();
+        assert.equal(dropdownPresent, true, 'available-indexes is not displayed');
+        await indexButton.click();
+
+        await datePickerBtn.click();
+        dropdownPresent = await driver.findElement(By.id("daterangepicker ")).isDisplayed();
+        assert.equal(dropdownPresent, true, 'daterangepicker is not displayed');
+        await datePickerBtn.click();
+
+        // await availableFieldBtn.click();
+        // dropdownPresent = await driver.findElement(By.id("available-fields")).isDisplayed();
+        // assert.equal(dropdownPresent, true, 'available-fields is not displayed');
+        // await availableFieldBtn.click();
+        console.log("All buttons tests passed")
+    }catch (err) {
+        // Handle any errors that occur during test execution
+        console.error(err);
+    
+    } finally {
+        // Close the driver instance, even if an error occurred
+        // await driver.quit();
+    }
+}
+
+testIndexPageButtons();
+
+async function testSearchInvalidQuery(){
+    try{
+        //To wait for browser to build and launch properly
+        driver = await new Builder().forBrowser("chrome")
+                                    .setChromeOptions(chromeOptions)
+                                    .build();
+        
+        //To fetch http://localhost/index.html from the browser with our code.
+        await driver.get("http://localhost/index.html");
+            
+        // Enter text in the search box
+        let searchBox = await driver.findElement(By.id("filter-input"));
+
+        // get the placeholder text using the getAttribute method
+        let placeholderText = await searchBox.getAttribute('placeholder');
+
+        // type the search query using the sendKeys method
+        // test invalid query
+        await searchBox.sendKeys('derf(1234');
+        await driver.sleep(500);
+        // submit the search by pressing Enter
+        await searchBox.sendKeys(Key.RETURN);
+        let cornerText = await driver.findElement(By.id("corner-text")).getText();
+        let closeBtn = await driver.findElement(By.id("close-btn"));
+        assert.equal(cornerText, 'Message: 1:5 (4): no match found, expected: "!=", ".", "<", "<=", "=", ">", ">=", "|", [ \\n\\t\\r], [-a-zA-Z0-9$&,?#%_@;[\\]{}+-./*:]i or EOF', "Invalid Query");
+
+        closeBtn.click();
+        await driver.sleep(5000);
+        // test query with no results
+        await searchBox.clear();
+        await searchBox.sendKeys('test');
+        await searchBox.sendKeys(Key.RETURN);
+
+        let emptyResponse = await driver.findElement(By.id("empty-response"));
+        let emptyResponseText = await emptyResponse.getText();
+
+        assert.equal(emptyResponseText, 'Your query returned no data, adjust your query.', "No results found for your query");
+        await driver.sleep(10000);
+        console.log("All search query test passed")
+
+    }catch (err) {
+        // Handle any errors that occur during test execution
+        console.error(err);
+    
+    } finally {
+        // Close the driver instance, even if an error occurred
+        await driver.quit();
+    }
+}
+
+testSearchInvalidQuery();
