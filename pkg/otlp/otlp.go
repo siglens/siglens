3a6fc354e9f151cadced7383efccbb9a7f991bc8
Andrew Hess <andrew7hess@gmail.com>
2023-11-02 00:39:29 -0600

    Ingest attribute keys as separate fields

--- pkg/otlp/otlp.go
+++ pkg/otlp/otlp.go
@@ -1,6 +1,7 @@
 package otlp
 
 import (
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 
@@ -9,52 +10,64 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/valyala/fasthttp"
 	coltracepb "go.opentelemetry.io/proto/otlp/collector/trace/v1"
+	commonpb "go.opentelemetry.io/proto/otlp/common/v1"
 	tracepb "go.opentelemetry.io/proto/otlp/trace/v1"
 	"google.golang.org/protobuf/proto"
 )
 
-type Span struct {
-	TraceId                []byte `json:"trace_id,omitempty"`
-	SpanId                 []byte `json:"span_id,omitempty"`
-	ParentSpanId           []byte `json:"parent_span_id,omitempty"`
-	TraceState             string `json:"trace_state,omitempty"`
-	Name                   string `json:"name,omitempty"`
-	Kind                   string `json:"kind,omitempty"`
-	StartTime              uint64 `json:"start_time,omitempty"`
-	EndTime                uint64 `json:"end_time,omitempty"`
-	Duration               uint64 `json:"duration,omitempty"`
-	Attributes             string `json:"attributes,omitempty"`
-	DroppedAttributesCount uint64 `json:"dropped_attributes_count,omitempty"`
-	Events                 string `json:"events,omitempty"`
-	DroppedEventsCount     uint64 `json:"dropped_events_count,omitempty"`
-	Links                  string `json:"links,omitempty"`
-	DroppedLinksCount      uint64 `json:"dropped_links_count,omitempty"`
-	Status                 string `json:"status,omitempty"`
-	Service                string `json:"service,omitempty"`
-}
+func spanToJson(span *tracepb.Span, service string) ([]byte, error) {
+	result := make(map[string]interface{})
+	result["trace_id"] = hex.EncodeToString(span.TraceId)
+	result["span_id"] = hex.EncodeToString(span.SpanId)
+	result["parent_span_id"] = hex.EncodeToString(span.ParentSpanId)
+	result["service"] = service
+	result["trace_state"] = span.TraceState
+	result["name"] = span.Name
+	result["kind"] = span.Kind.String()
+	result["start_time"] = span.StartTimeUnixNano
+	result["end_time"] = span.EndTimeUnixNano
+	result["duration"] = span.EndTimeUnixNano - span.StartTimeUnixNano
+	result["dropped_attributes_count"] = uint64(span.DroppedAttributesCount)
+	result["dropped_events_count"] = uint64(span.DroppedEventsCount)
+	result["dropped_links_count"] = uint64(span.DroppedLinksCount)
+	result["status"] = span.Status.String()
 
-func toSpan(otlpSpan *tracepb.Span) *Span {
-	span := &Span{
-		TraceId:                otlpSpan.TraceId,
-		SpanId:                 otlpSpan.SpanId,
-		ParentSpanId:           otlpSpan.ParentSpanId,
-		TraceState:             otlpSpan.TraceState,
-		Name:                   otlpSpan.Name,
-		Kind:                   otlpSpan.Kind.String(),
-		StartTime:              otlpSpan.StartTimeUnixNano,
-		EndTime:                otlpSpan.EndTimeUnixNano,
-		Duration:               otlpSpan.EndTimeUnixNano - otlpSpan.StartTimeUnixNano,
-		Attributes:             "TODO", // otlpSpan.Attributes,
-		DroppedAttributesCount: uint64(otlpSpan.DroppedAttributesCount),
-		Events:                 "TODO", // otlpSpan.Events,
-		DroppedEventsCount:     uint64(otlpSpan.DroppedEventsCount),
-		Links:                  "TODO", // otlpSpan.Links,
-		DroppedLinksCount:      uint64(otlpSpan.DroppedLinksCount),
-		Status:                 "TODO", // otlpSpan.Status,
-		Service:                "TODO",
+	// Make a column for each attribute key.
+	for _, keyvalue := range span.Attributes {
+		key := keyvalue.Key
+
+		switch keyvalue.Value.Value.(type) {
+		case *commonpb.AnyValue_StringValue:
+			result[key] = keyvalue.Value.GetStringValue()
+		case *commonpb.AnyValue_IntValue:
+			result[key] = keyvalue.Value.GetIntValue()
+		case *commonpb.AnyValue_DoubleValue:
+			result[key] = keyvalue.Value.GetDoubleValue()
+		case *commonpb.AnyValue_BoolValue:
+			result[key] = keyvalue.Value.GetBoolValue()
+		default:
+			return nil, fmt.Errorf("spanToJson: unsupported value type in attribuates: %T", keyvalue.Value.Value)
+		}
 	}
 
-	return span
+	var err error
+	result["events_original"] = span.Events // deleteme
+	result["events"], err = json.Marshal(span.Events)
+	if err != nil {
+		return nil, err
+	}
+
+	result["links_original"] = span.Links // deleteme
+	result["links"], err = json.Marshal(span.Links)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Errorf("result before marshal: %v", result)
+
+	bytes, err := json.Marshal(result)
+	log.Errorf("result after marshal: %s", bytes)
+	return bytes, err
 }
 
 func unpackTrace(data []byte) (*coltracepb.ExportTraceServiceRequest, error) {
@@ -89,12 +102,23 @@ func ProcessTraceIngest(ctx *fasthttp.RequestCtx) {
 	orgId := uint64(0)
 
 	for _, resourceSpans := range request.ResourceSpans {
+		// Find the service name.
+		var service string
+		if resourceSpans.Resource != nil {
+			for _, keyvalue := range resourceSpans.Resource.Attributes {
+				if keyvalue.Key == "service.name" {
+					service = keyvalue.Value.GetStringValue()
+				}
+			}
+		}
+
+		// Ingest each of these spans.
 		for _, scopeSpans := range resourceSpans.ScopeSpans {
 			for _, span := range scopeSpans.Spans {
 				// log.Errorf("got span: %s", span)
 				// spans.Spans = append(spans.Spans, span.String())
 
-				jsonData, err := json.Marshal(span)
+				jsonData, err := spanToJson(span, service)
 				if err != nil {
 					log.Errorf("processTraceIngest: failed to marshal spans: %v", err)
 					// TODO: return error
@@ -112,6 +136,10 @@ func ProcessTraceIngest(ctx *fasthttp.RequestCtx) {
 	}
 
 	response, err := proto.Marshal(&coltracepb.ExportTraceServiceResponse{})
+	if err != nil {
+		log.Errorf("processTraceIngest failed to marshal response: %v", err)
+		// TODO: return error
+	}
 	_, err = ctx.Write(response)
 	if err != nil {
 		log.Errorf("processTraceIngest failed to write response: %v", err)
