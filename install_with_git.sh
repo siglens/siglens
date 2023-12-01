1ba408b42e280b620c6a345251f560dac55ee47b
tcefrep <67371917+asiL-tcefreP@users.noreply.github.com>
2023-12-01 18:26:13 -0500

    Use Segment to record install statistics (#184)
    * Use computer-specific identifier as user id
    
    * Add install statistics
    
    * Fix up
    
    ---------
    
    Co-authored-by: Andrew Hess <ahess@sigscalr.io>

--- install_with_git.sh
+++ install_with_git.sh
@@ -24,8 +24,35 @@ display_step 3 "Changing directory and downloading Go dependencies"
 cd siglens
 go mod tidy
 
+# Step 4: Install statistics
+display_step 4 "Install statistics"
+# Extract the first occurrence of a valid MAC address
+computer_specific_identifier=$(ifconfig 2>/dev/null | grep -o -E '([0-9a-fA-F]{2}:){5}([0-9a-fA-F]{2})' | head -n 1)
+
+# If it can not get the mac address, use hostname as computer-specific identifier 
+if [ -z "$computer_specific_identifier" ]; then
+  computer_specific_identifier=$(hostname)
+fi
+
+# Get OS information
+runtime_os=$(uname)
+runtime_arch=$(uname -m)
+
+curl -X POST \
+https://api.segment.io/v1/track \
+-H 'Content-Type: application/json' \
+-H 'Authorization: Basic QlBEam5lZlBWMEpjMkJSR2RHaDdDUVRueWtZS2JEOGM6' \
+-d '{
+  "userId": "'"$computer_specific_identifier"'",
+  "event": "install (not running)",
+  "properties": {
+    "runtime_arch": "'"$runtime_os"'",
+    "runtime_os": "'"$runtime_arch"'"
+  }
+}'
+
 PORT=80 
-display_step 4 "Running the Server on http://localhost:$PORT"
+display_step 5 "Running the Server on http://localhost:$PORT"
 if [ $PORT == 80 ]; then 
     echo "If you are not able to access localhost:80, try running '"export PORT=8090"' and rerunning this script"
 fi
