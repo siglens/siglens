9af7534018130bcbb47efa5a5250a7fe51db3161
Zhifei-Li <113303904+Zhifei-Li@users.noreply.github.com>
2023-12-07 00:05:37 -0500

    zoom graph

--- static/js/dependency-graph.js
+++ static/js/dependency-graph.js
@@ -107,86 +107,133 @@ function createDependencyMatrix(res) {
 }
 
 function displayDependencyGraph(nodes, links) {
-    // Create SVG container
-    const svg = d3
-        .select("#dependency-graph-container")
-        .append("svg")
-        .append("g")
-        .attr("transform","translate(" + svgWidth / 4 + "," + svgHeight / 4 + ")");
+  const svg = d3
+    .select("#dependency-graph-container")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .call(d3.zoom().on("zoom", zoomed))
+    .append("g")
+    .attr("transform", "translate(" + svgWidth / 4 + "," + svgHeight / 4 + ")");
 
-    // Create force simulation
-    const simulation = d3
-        .forceSimulation(nodes)
-        .force("link",d3.forceLink(links).id((d) => d.id).distance(200).strength(0.5))
-        .force("charge", d3.forceManyBody().strength(-300))
-        .force("center", d3.forceCenter(250, 250))
-        .force("radial", d3.forceRadial(250, 250, 250).strength(0.1));
+  const simulation = d3
+    .forceSimulation(nodes)
+    .force(
+      "link",
+      d3
+        .forceLink(links)
+        .id((d) => d.id)
+        .distance(200)
+        .strength(0.5)
+    )
+    .force("charge", d3.forceManyBody().strength(-300))
+    .force("center", d3.forceCenter(250, 250))
+    .force("radial", d3.forceRadial(250, 250, 250).strength(0.1));
 
-    // Arrow Head
-    svg.append("defs")
-        .append("marker")
-        .attr("id", "arrowhead")
-        .attr("viewBox", "-0 -5 10 10")
-        .attr("refX", 23)
-        .attr("refY", 0)
-        .attr("orient", "auto")
-        .attr("markerWidth", 12)
-        .attr("markerHeight", 12)
-        .attr("xoverflow", "visible")
-        .append("svg:path")
-        .attr("d", "M 0,-5 L 10 ,0 L 0,5");
+  svg
+    .append("defs")
+    .append("marker")
+    .attr("id", "arrowhead")
+    .attr("viewBox", "-0 -5 10 10")
+    .attr("refX", 23)
+    .attr("refY", 0)
+    .attr("orient", "auto")
+    .attr("markerWidth", 12)
+    .attr("markerHeight", 12)
+    .attr("xoverflow", "visible")
+    .append("svg:path")
+    .attr("d", "M 0,-5 L 10 ,0 L 0,5");
 
-    // Create links
-    const link = svg
-        .selectAll(".links")
-        .data(links)
-        .enter()
-        .append("line")
-        .attr("class", "links line")
-        .attr("marker-end", "url(#arrowhead)");
+  const link = svg
+    .selectAll(".links")
+    .data(links)
+    .enter()
+    .append("line")
+    .attr("class", "links line")
+    .attr("marker-end", "url(#arrowhead)");
 
-    // Create nodes
-    const node = svg
-        .selectAll("circle")
-        .data(nodes)
-        .enter()
-        .append("circle")
-        .attr("r", 20) //radius of node
-        .attr("fill", (d, i) => colorArray[i % colorArray.length]);
+  const node = svg
+    .selectAll("circle")
+    .data(nodes)
+    .enter()
+    .append("circle")
+    .attr("r", 20)
+    .attr("fill", (d, i) => colorArray[i % colorArray.length])
+    .call(
+      d3
+        .drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended)
+    );
 
-    // Add labels for nodes
-    const label = svg
-        .selectAll(".label")
-        .data(nodes)
-        .enter()
-        .append("text")
-        .text((d) => d.id)
-        .attr("class", "label");
+  const label = svg
+    .selectAll(".label")
+    .data(nodes)
+    .enter()
+    .append("text")
+    .text((d) => d.id)
+    .attr("class", "label");
 
-    // Add labels for links
-    const linkLabel = svg
-        .selectAll(".link-label")
-        .data(links)
-        .enter()
-        .append("text")
-        .text((d) => d.value)
-        .attr("class", "link-label")
-        .attr("dy", -10)
-        .attr("dx", -10);
+  const linkLabel = svg
+    .selectAll(".link-label")
+    .data(links)
+    .enter()
+    .append("text")
+    .text((d) => d.value)
+    .attr("class", "link-label")
+    .attr("dy", -10)
+    .attr("dx", -10);
 
-    // Update positions on each tick
-    simulation.on("tick", () => {
-        link.attr("x1", (d) => d.source.x)
-            .attr("y1", (d) => d.source.y)
-            .attr("x2", (d) => d.target.x)
-            .attr("y2", (d) => d.target.y);
+  simulation.on("tick", () => {
+    link
+      .attr("x1", (d) => d.source.x)
+      .attr("y1", (d) => d.source.y)
+      .attr("x2", (d) => d.target.x)
+      .attr("y2", (d) => d.target.y);
 
-        node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+    node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
 
-        label.attr("x", (d) => d.x - 10).attr("y", (d) => d.y - 25);
+    label.attr("x", (d) => d.x - 10).attr("y", (d) => d.y - 25);
 
-        linkLabel
-            .attr("x", (d) => (d.source.x + d.target.x) / 2)
-            .attr("y", (d) => (d.source.y + d.target.y) / 2);
-    });
+    linkLabel
+      .attr(
+        "x",
+        (d) => (d.source.x + d.target.x) / 2 - (d.source.x - d.target.x) / 2
+      )
+      .attr(
+        "y",
+        (d) => (d.source.y + d.target.y) / 2 - (d.source.y - d.target.y) / 2
+      );
+  });
+
+  function zoomed(event) {
+    // Update the transform attribute with a translation that keeps the center fixed during scaling
+    const newTransform = d3.zoomIdentity
+      .translate(svgWidth / 4, svgHeight / 4) // Adjust these values based on your desired center
+      .scale(event.transform.k);
+
+    svg.attr("transform", newTransform);
+  }
+
+  function dragstarted(event, d) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    d.initialPos = { x: d.x, y: d.y };
+  }
+
+  function dragged(event, d) {
+    // Calculate the offset between the initial position and the current mouse position
+    const offsetX = event.x - d.initialPos.x;
+    const offsetY = event.y - d.initialPos.y;
+
+    // Update the fixed position based on the offset
+    d.fx = d.initialPos.x + offsetX;
+    d.fy = d.initialPos.y + offsetY;
+  }
+
+  function dragended(event, d) {
+    if (!event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
 }
