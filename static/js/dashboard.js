4f004a17c1b93bc500ed511bce15685895a5f471
Pratiksha Patil <pratikshabpatil010@gmail.com>
2023-12-18 12:13:12 +0530

    Replaced alert by popupOverlay

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1131,25 +1131,33 @@ function addDbSettingsEventListeners() {
 
 function saveDbSetting() {
     let trimmedDbName = $('.dbSet-dbName').val().trim();
-    
+
     if (!trimmedDbName) {
-        // Show error message or handle the case where the name is empty
-        alert('Dashboard name cannot be empty or consist of only spaces.');
+        // Show error message using error-tip and popupOverlay
+        $('.error-tip').addClass('active');
+        $('.popupOverlay, .popupContent').addClass('active');
+        $('#error-message').text('Dashboard name cannot be empty.');
         return;
     }
 
     $('.dbSet-dbName').val("");
     $('.dbSet-dbDescr').val("");
     $('.dbSet-jsonModelData').val("");
-// Update dashboard with the trimmed name
-dbName = trimmedDbName;
-dbDescr = dbData.description;
+
+    dbName = trimmedDbName;
+    dbDescr = dbData.description;
 
 
     updateDashboard();
     $('#app-container').show();
     $('.dbSet-container').hide();
+    $('#dbSet-discard').click();
 }
+
+$('#error-ok-btn').click(function () {
+    $('.popupOverlay, .popupContent').removeClass('active');
+    $('.error-tip').removeClass('active');
+});
 
 function discardDbSetting() {
     if(editPanelFlag){
