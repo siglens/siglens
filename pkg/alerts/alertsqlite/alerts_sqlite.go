cdced1a943b7fc83d1bed3bec850722a83e706f7
Venkatesha Matam <113487319+venkateshamatam@users.noreply.github.com>
2023-12-22 10:25:55 -0500

    Feature: API to implement alert silencing (#247)
    * FIX dashboard name refresh
    
    * FIX Display an empty drop-down
    
    * add tooltip to alert bell icon
    
    * Revert "add tooltip to alert bell icon"
    
    This reverts commit e8459774f6b8d2e2e4f0848b8899b369724e4c9c.
    
    * Created API to silence alerts
    
    * functions to implement silence period
    
    * function to delay notifications until silence period is over
    
    * error handling
    
    * UPDATE SilencePeriod to SilenceMinutes
    
    ---------
    
    Co-authored-by: venkateshamatam <>

--- pkg/alerts/alertsqlite/alerts_sqlite.go
+++ pkg/alerts/alertsqlite/alerts_sqlite.go
@@ -54,8 +54,8 @@ const allAlertsTableQuery = `CREATE TABLE IF NOT EXISTS siglens.all_alerts (
 	contact_name TEXT NOT NULL,
 	cron_job JSONB,
 	labels JSONB,
-	node_id INT
-
+	node_id INT,
+	silence_minutes INT DEFAULT 0
   );`
 
 const minionSearchesTableQuery = `CREATE TABLE IF NOT EXISTS siglens.minion_searches (
@@ -376,7 +376,7 @@ func (p Sqlite) GetAllAlerts() ([]alertutils.AlertInfo, error) {
 		log.Errorf("getAllAlerts: unable to begin transaction, err: %+v", err)
 		return nil, err
 	}
-	sqlStatement := "SELECT alert_id, alert_name, state, create_timestamp, contact_id, labels FROM all_alerts;"
+	sqlStatement := "SELECT alert_id, alert_name, state, create_timestamp, contact_id, labels, silence_minutes FROM all_alerts;"
 	rows, err := tx.Query(sqlStatement)
 	if err != nil {
 		log.Errorf("getAllAlerts: unable to execute query: %v, err: %+v", sqlStatement, err)
@@ -392,8 +392,9 @@ func (p Sqlite) GetAllAlerts() ([]alertutils.AlertInfo, error) {
 			create_timestamp time.Time
 			contact_id       string
 			labels           []byte
+			silence_minutes  uint64
 		)
-		err := rows.Scan(&alert_id, &alert_name, &state, &create_timestamp, &contact_id, &labels)
+		err := rows.Scan(&alert_id, &alert_name, &state, &create_timestamp, &contact_id, &labels, &silence_minutes)
 		if err != nil {
 			log.Errorf("getAllAlerts: uanble to scan row: %+v", err)
 			_ = tx.Rollback()
@@ -407,7 +408,7 @@ func (p Sqlite) GetAllAlerts() ([]alertutils.AlertInfo, error) {
 			return nil, err
 		}
 
-		alerts = append(alerts, alertutils.AlertInfo{AlertId: alert_id, AlertName: alert_name, State: state, CreateTimestamp: create_timestamp, ContactId: contact_id, Labels: labels_array})
+		alerts = append(alerts, alertutils.AlertInfo{AlertId: alert_id, AlertName: alert_name, State: state, CreateTimestamp: create_timestamp, ContactId: contact_id, Labels: labels_array, SilenceMinutes: silence_minutes})
 	}
 	err = tx.Commit()
 	if err != nil {
@@ -415,6 +416,40 @@ func (p Sqlite) GetAllAlerts() ([]alertutils.AlertInfo, error) {
 		return nil, err
 	}
 	return alerts, nil
+}
+func (p Sqlite) UpdateSilenceMinutes(updatedSilenceMinutes *alertutils.AlertDetails) error {
+	if !isValid(updatedSilenceMinutes.AlertInfo.AlertName) || !isValid(updatedSilenceMinutes.AlertInfo.ContactName) || !isValid(updatedSilenceMinutes.QueryParams.QueryText) {
+		log.Errorf("updateSilenceMinutes: data validation check failed")
+		return errors.New("updateSilenceMinutesupdateSilenceMinutes: data validation check failed")
+	}
+	alertExists, _, err := p.verifyAlertExists(updatedSilenceMinutes.AlertInfo.AlertId)
+	if err != nil {
+		log.Errorf("updateSilenceMinutes: unable to verify if alert exists, err: %+v", err)
+		return err
+	}
+	if !alertExists {
+		log.Errorf("updateSilenceMinutes: alert does not exist")
+		return errors.New("alert does not exist")
+	}
+
+	tx, err := p.db.BeginTx(p.ctx, nil)
+	if err != nil {
+		log.Errorf("updateSilenceMinutes: unable to begin transaction, err: %+v", err)
+		return err
+	}
+	sqlStatement := "UPDATE all_alerts SET silence_minutes=$1 WHERE alert_id=$2;"
+	_, err = tx.ExecContext(p.ctx, sqlStatement, updatedSilenceMinutes.AlertInfo.SilenceMinutes, updatedSilenceMinutes.AlertInfo.AlertId)
+	if err != nil {
+		log.Errorf("updateSilenceMinutes: unable to execute query: %v, with alert name: %v, err: %+v", sqlStatement, updatedSilenceMinutes.AlertInfo.AlertName, err)
+		_ = tx.Rollback()
+		return err
+	}
+	err = tx.Commit()
+	if err != nil {
+		log.Errorf("updateSilenceMinutes: unable to execute transaction, err: %+v", err)
+		return err
+	}
+	return nil
 }
 
 // Deletes cron job associated with the alert
