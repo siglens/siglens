9ebb3311499fd9b3a25b133aff390232c59cb148
sonamgupta21 <sonam2001gupta@gmail.com>
2024-01-08 17:52:25 +0530

    Added Upper Navbar Component

---
+++ static/component/upper-navbar/upper-navbar.js
@@ -0,0 +1,53 @@
+(function ($) {
+    $.fn.orgNavTabs = function () {
+        return this.each(function () {
+            var $container = $(this);
+            var $tabs = $container.find('.section-button');
+
+            $container.on('click', '.section-button', function (e) {
+                e.preventDefault();
+                $tabs.removeClass('active');
+                $(this).addClass('active');
+                var href = $(this).find('a').attr('href');
+                window.location.href = href;
+            });
+
+            // Add active class based on the current URL
+            var currentUrl = window.location.href;
+            var currentTab = extractLastPathSegment(currentUrl);
+            $tabs.removeClass('active');
+            if (currentTab === 'all-alerts' || currentTab === 'alert' || currentTab === 'alert-details') {
+                $tabs.filter('[id="all-alerts"]').addClass('active');
+            } else {
+                $tabs.filter(`[id="${currentTab}"]`).addClass('active');
+            }
+        
+        });
+    };
+
+    function extractLastPathSegment(url) {
+        return (new URL(url).pathname.match(/[^/]+\/?$/)[0] || '').replace(/\..+$/, '');
+    }
+
+    $.fn.appendOrgNavTabs = function (header,buttonArray) {
+        var htmlBlock = `
+            <div>
+                <h1 class="myOrg-heading">${header}</h1>
+                <div class="section-buttons">
+        `;
+
+        buttonArray.forEach(function (button) {
+            htmlBlock += `
+                <div class="section-button" id="${button.class}"><a href="${button.url}">${button.name}</a></div>
+            `;
+        });
+
+        htmlBlock += `
+                </div>
+            </div>
+        `;
+
+        this.append(htmlBlock);
+        this.orgNavTabs();
+    };
+})(jQuery);
