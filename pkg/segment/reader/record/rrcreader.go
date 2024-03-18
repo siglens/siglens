19640403cacc75cfb967045f30134966db386065
Andrew Hess <andrew7hess@gmail.com>
2024-03-18 15:16:18 -0600

    Separate processSiglenSegment into a closure

--- pkg/segment/reader/record/rrcreader.go
+++ pkg/segment/reader/record/rrcreader.go
@@ -188,145 +188,149 @@ func GetJsonFromAllRrc(allrrc []*utils.RecordResultContainer, esResponse bool, q
 	recsAggRecords := make([]map[string]interface{}, 0)
 	var numTotalSegments uint64
 
+	processSingleSegment := func(currSeg string, virtualTableName string, blkRecIndexes map[uint16]map[uint16]uint64) {
+		recs, cols, err := GetRecordsFromSegment(currSeg, virtualTableName, blkRecIndexes,
+			config.GetTimeStampKey(), esResponse, qid, aggs)
+		if err != nil {
+			log.Errorf("GetJsonFromAllRrc: failed to read recs from segfile=%v, err=%v", currSeg, err)
+			return
+		}
+		for cName := range cols {
+			finalCols[cName] = true
+		}
+
+		for key := range renameHardcodedColumns {
+			finalCols[key] = true
+		}
+
+		if hasQueryAggergatorBlock || transactionArgsExist {
+
+			numTotalSegments, err = query.GetTotalSegmentsToSearch(qid)
+			if err != nil {
+				// For synchronous queries, the query is deleted by this
+				// point, but segmap has all the segments that the query
+				// searched.
+				// For async queries, the segmap has just one segment
+				// because we process them as the search completes, but the
+				// query isn't deleted until all segments get processed, so
+				// we shouldn't get to this block for async queries.
+				numTotalSegments = uint64(len(segmap))
+			}
+
+			/**
+			* Overview of Aggregation Processing:
+			* 1. Initiate the process by executing PostQueryBucketCleaning to prepare records for aggregation.
+			* 2. Evaluate the PerformAggsOnRecs flag post-cleanup:
+			*    - True: Indicates not all aggregations were processed. In this case:
+			*       a. Perform aggregations on records using performAggsOnRecs. This function requires all the segments to be processed before proceeding to the next step.
+			*       b. Evaluate the CheckNextAgg flag from the result:
+			*          i. If true, reset PerformAggsOnRecs to false, update aggs with NextQueryAgg, and loop for additional cleaning.
+			*          ii. If false or if resultRecMap is empty, it implies additional segments may require processing; exit the loop for further segment evaluation.
+			*    - False: All aggregations for the current segment have been processed; exit the loop to either process the next segment or return the final results.
+			* 3. The loop facilitates sequential data processing, ensuring each or all the segments are thoroughly processed before proceeding to the next,
+			*    adapting dynamically based on the flags set by the PostQueryBucketCleaning and PerformAggsOnRecs functions.
+			 */
+			for {
+				agg.PostQueryBucketCleaning(nodeRes, aggs, recs, recordIndexInFinal, finalCols, numTotalSegments)
+				if nodeRes.PerformAggsOnRecs {
+					resultRecMap = search.PerformAggsOnRecs(nodeRes, aggs, recs, finalCols, numTotalSegments, qid)
+					if len(resultRecMap) > 0 {
+						boolVal, exists := resultRecMap["CHECK_NEXT_AGG"]
+						if exists && boolVal {
+							// Reset the flag to false and update aggs with NextQueryAgg to loop for additional cleaning.
+							nodeRes.PerformAggsOnRecs = false
+							aggs = nodeRes.NextQueryAgg
+						} else {
+							break
+						}
+					} else {
+						// Not checking or processing Next Agg. This implies that there might be more segments to process.
+						// Break out of the loop and continue processing the next segment.
+						break
+					}
+				} else {
+					// No need to perform aggs on recs. All the Aggs are Processed.
+					break
+				}
+			}
+			// For other cmds, if we cannot map recInden to an index, we simply append the record to allRecords
+			// However, for the sort cmd, we should assign the length of the result set to be the same as recordIndexInFinal
+			// This way, when mapping the results to allRecords, we can preserve the order of the results rather than just appending them to the end of allRecords
+			if len(recordIndexInFinal) > len(allRecords) {
+				allRecords = make([]map[string]interface{}, len(recordIndexInFinal))
+			}
+		}
+
+		numProcessedRecords += len(recs)
+		for recInden, record := range recs {
+			for key, val := range renameHardcodedColumns {
+				record[key] = val
+			}
+
+			unknownIndex := false
+			idx, ok := recordIndexInFinal[recInden]
+			if !ok {
+				// For async queries where we need all records before we
+				// can return any (like dedup with a sortby), once we can
+				// get to this block because processing the dedup may
+				// return some records from previous segments and since
+				// it's an async query we're running this function with
+				// len(segmap)=1 because we try to process the data as the
+				// searched complete.
+				log.Infof("qid=%d, GetJsonFromAllRrc: Did not find index for record indentifier %s.", qid, recInden)
+				unknownIndex = true
+			}
+			if logfmtRequest {
+				record = addKeyValuePairs(record)
+			}
+			includeValues := make(map[string]interface{})
+			for cname, val := range record {
+				if len(valuesToLabels[cname]) > 0 {
+					actualIndex := rawIncludeValuesIndicies[cname]
+					switch valType := val.(type) {
+					case []interface{}:
+						if actualIndex > len(valType)-1 || actualIndex < 0 {
+							log.Errorf("GetJsonFromAllRrc: index=%v out of bounds for column=%v of length %v", actualIndex, cname, len(valType))
+							continue
+						}
+						includeValues[valuesToLabels[cname]] = valType[actualIndex]
+					case interface{}:
+						log.Errorf("GetJsonFromAllRrc: accessing object in %v as array!", cname)
+						continue
+					default:
+						log.Errorf("GetJsonFromAllRrc: unsupported value type")
+						continue
+					}
+				}
+
+			}
+			for label, val := range includeValues {
+				if record[label] != nil {
+					log.Errorf("GetJsonFromAllRrc: accessing object in %v as array!", label) //case where label == original column
+					continue
+				}
+				record[label] = val
+			}
+
+			delete(recordIndexInFinal, recInden)
+
+			if unknownIndex {
+				allRecords = append(allRecords, record)
+			} else {
+				allRecords[idx] = record
+			}
+
+			if transactionArgsExist {
+				recsAggRecords = append(recsAggRecords, record)
+			}
+		}
+	}
+
 	if !(tableColumnsExist || aggs.OutputTransforms == nil || hasQueryAggergatorBlock || transactionArgsExist) {
 		allRecords, finalCols = applyHardcodedColumns(hardcodedArray, renameHardcodedColumns, allRecords, finalCols)
 	} else {
 		for currSeg, blkIds := range segmap {
-			recs, cols, err := GetRecordsFromSegment(currSeg, blkIds.VirtualTableName, blkIds.BlkRecIndexes,
-				config.GetTimeStampKey(), esResponse, qid, aggs)
-			if err != nil {
-				log.Errorf("GetJsonFromAllRrc: failed to read recs from segfile=%v, err=%v", currSeg, err)
-				continue
-			}
-			for cName := range cols {
-				finalCols[cName] = true
-			}
-
-			for key := range renameHardcodedColumns {
-				finalCols[key] = true
-			}
-
-			if hasQueryAggergatorBlock || transactionArgsExist {
-
-				numTotalSegments, err = query.GetTotalSegmentsToSearch(qid)
-				if err != nil {
-					// For synchronous queries, the query is deleted by this
-					// point, but segmap has all the segments that the query
-					// searched.
-					// For async queries, the segmap has just one segment
-					// because we process them as the search completes, but the
-					// query isn't deleted until all segments get processed, so
-					// we shouldn't get to this block for async queries.
-					numTotalSegments = uint64(len(segmap))
-				}
-
-				/**
-				* Overview of Aggregation Processing:
-				* 1. Initiate the process by executing PostQueryBucketCleaning to prepare records for aggregation.
-				* 2. Evaluate the PerformAggsOnRecs flag post-cleanup:
-				*    - True: Indicates not all aggregations were processed. In this case:
-				*       a. Perform aggregations on records using performAggsOnRecs. This function requires all the segments to be processed before proceeding to the next step.
-				*       b. Evaluate the CheckNextAgg flag from the result:
-				*          i. If true, reset PerformAggsOnRecs to false, update aggs with NextQueryAgg, and loop for additional cleaning.
-				*          ii. If false or if resultRecMap is empty, it implies additional segments may require processing; exit the loop for further segment evaluation.
-				*    - False: All aggregations for the current segment have been processed; exit the loop to either process the next segment or return the final results.
-				* 3. The loop facilitates sequential data processing, ensuring each or all the segments are thoroughly processed before proceeding to the next,
-				*    adapting dynamically based on the flags set by the PostQueryBucketCleaning and PerformAggsOnRecs functions.
-				 */
-				for {
-					agg.PostQueryBucketCleaning(nodeRes, aggs, recs, recordIndexInFinal, finalCols, numTotalSegments)
-					if nodeRes.PerformAggsOnRecs {
-						resultRecMap = search.PerformAggsOnRecs(nodeRes, aggs, recs, finalCols, numTotalSegments, qid)
-						if len(resultRecMap) > 0 {
-							boolVal, exists := resultRecMap["CHECK_NEXT_AGG"]
-							if exists && boolVal {
-								// Reset the flag to false and update aggs with NextQueryAgg to loop for additional cleaning.
-								nodeRes.PerformAggsOnRecs = false
-								aggs = nodeRes.NextQueryAgg
-							} else {
-								break
-							}
-						} else {
-							// Not checking or processing Next Agg. This implies that there might be more segments to process.
-							// Break out of the loop and continue processing the next segment.
-							break
-						}
-					} else {
-						// No need to perform aggs on recs. All the Aggs are Processed.
-						break
-					}
-				}
-				// For other cmds, if we cannot map recInden to an index, we simply append the record to allRecords
-				// However, for the sort cmd, we should assign the length of the result set to be the same as recordIndexInFinal
-				// This way, when mapping the results to allRecords, we can preserve the order of the results rather than just appending them to the end of allRecords
-				if len(recordIndexInFinal) > len(allRecords) {
-					allRecords = make([]map[string]interface{}, len(recordIndexInFinal))
-				}
-			}
-
-			numProcessedRecords += len(recs)
-			for recInden, record := range recs {
-				for key, val := range renameHardcodedColumns {
-					record[key] = val
-				}
-
-				unknownIndex := false
-				idx, ok := recordIndexInFinal[recInden]
-				if !ok {
-					// For async queries where we need all records before we
-					// can return any (like dedup with a sortby), once we can
-					// get to this block because processing the dedup may
-					// return some records from previous segments and since
-					// it's an async query we're running this function with
-					// len(segmap)=1 because we try to process the data as the
-					// searched complete.
-					log.Infof("qid=%d, GetJsonFromAllRrc: Did not find index for record indentifier %s.", qid, recInden)
-					unknownIndex = true
-				}
-				if logfmtRequest {
-					record = addKeyValuePairs(record)
-				}
-				includeValues := make(map[string]interface{})
-				for cname, val := range record {
-					if len(valuesToLabels[cname]) > 0 {
-						actualIndex := rawIncludeValuesIndicies[cname]
-						switch valType := val.(type) {
-						case []interface{}:
-							if actualIndex > len(valType)-1 || actualIndex < 0 {
-								log.Errorf("GetJsonFromAllRrc: index=%v out of bounds for column=%v of length %v", actualIndex, cname, len(valType))
-								continue
-							}
-							includeValues[valuesToLabels[cname]] = valType[actualIndex]
-						case interface{}:
-							log.Errorf("GetJsonFromAllRrc: accessing object in %v as array!", cname)
-							continue
-						default:
-							log.Errorf("GetJsonFromAllRrc: unsupported value type")
-							continue
-						}
-					}
-
-				}
-				for label, val := range includeValues {
-					if record[label] != nil {
-						log.Errorf("GetJsonFromAllRrc: accessing object in %v as array!", label) //case where label == original column
-						continue
-					}
-					record[label] = val
-				}
-
-				delete(recordIndexInFinal, recInden)
-
-				if unknownIndex {
-					allRecords = append(allRecords, record)
-				} else {
-					allRecords[idx] = record
-				}
-
-				if transactionArgsExist {
-					recsAggRecords = append(recsAggRecords, record)
-				}
-			}
+			processSingleSegment(currSeg, blkIds.VirtualTableName, blkIds.BlkRecIndexes)
 		}
 	}
 
