d343a9aa8ed90bf898168cae0171f5b3c4648035
Macbeth98 <manichandra.teja@gmail.com>
2024-03-19 18:59:39 -0400

    chore: comment correction

--- pkg/config/common/common.go
+++ pkg/config/common/common.go
@@ -33,7 +33,7 @@ type LogConfig struct {
 }
 
 type TLSConfig struct {
-	Enabled         bool   `yaml:"enabled"`         // folder to store acme certificates
+	Enabled         bool   `yaml:"enabled"`         // enable/disable tls
 	CertificatePath string `yaml:"certificatePath"` // path to certificate file
 	PrivateKeyPath  string `yaml:"privateKeyPath"`  // path to private key file
 }
