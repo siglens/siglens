929973527a79aa24d206c4a441586ec24dc40f37
Ven <vmatam@Venkys-MacBook-Pro.local>
2024-04-11 10:27:17 -0400

    add install_age

--- pkg/ssa/ssa.go
+++ pkg/ssa/ssa.go
@@ -22,6 +22,7 @@ import (
 	"math"
 	"net/http"
 	"os"
+	"path"
 	"runtime"
 	"strconv"
 	"strings"
@@ -32,7 +33,9 @@ import (
 	"github.com/shirou/gopsutil/v3/mem"
 	"github.com/siglens/siglens/pkg/config"
 	"github.com/siglens/siglens/pkg/localnodeid"
+	"github.com/siglens/siglens/pkg/segment/writer"
 	segwriter "github.com/siglens/siglens/pkg/segment/writer"
+	mmeta "github.com/siglens/siglens/pkg/segment/writer/metrics/meta"
 	"github.com/siglens/siglens/pkg/usageStats"
 	"github.com/siglens/siglens/pkg/utils"
 	"github.com/siglens/siglens/pkg/virtualtable"
@@ -124,9 +127,64 @@ func InitSsa() {
 
 	IPAddressInfo = ipDetails
 	client = currClient
+
+	timestampFilePath := path.Join(config.GetDataPath(), "timestamp.txt")
+	_, err = os.Stat(timestampFilePath)
+	if os.IsNotExist(err) {
+		// If the file does not exist, get the oldest segment epoch
+		oldestSegmentEpoch, err := GetOldestSegmentEpoch(config.GetCurrentNodeIngestDir(), 0)
+		if err != nil || oldestSegmentEpoch == 0 || oldestSegmentEpoch == uint64(math.MaxUint64) {
+			// If there's an error getting the oldest segment epoch or it's 0, use the current timestamp
+			oldestSegmentEpoch = uint64(time.Now().UnixMilli())
+		}
+
+		// Write the timestamp to the file
+		err = os.WriteFile(timestampFilePath, []byte(strconv.FormatInt(int64(oldestSegmentEpoch), 10)), 0644)
+		if err != nil {
+			log.Fatalf("InitSsa: Failed to write timestamp to file: %v", err)
+		}
+	} else if err != nil {
+		log.Fatalf("InitSsa: Failed to check if timestamp file exists: %v", err)
+	}
+
 	go waitForInitialEvent()
 }
 
+func GetOldestSegmentEpoch(ingestNodeDir string, orgid uint64) (uint64, error) {
+	// Read segmeta entries
+	currentSegmeta := path.Join(ingestNodeDir, writer.SegmetaSuffix)
+	allSegMetas, err := writer.ReadSegmeta(currentSegmeta)
+	if err != nil {
+		log.Errorf("GetOldestSegmentEpoch: Failed to read segmeta, err: %v", err)
+		return 0, err
+	}
+
+	// Read metrics meta entries
+	currentMetricsMeta := path.Join(ingestNodeDir, mmeta.MetricsMetaSuffix)
+	allMetricMetas, err := mmeta.ReadMetricsMeta(currentMetricsMeta)
+	if err != nil {
+		log.Errorf("GetOldestSegmentEpoch: Failed to get all metric meta entries, err: %v", err)
+		return 0, err
+	}
+
+	// Combine metrics and segments
+	oldest := uint64(math.MaxUint64)
+	for _, segMeta := range allSegMetas {
+		if segMeta.OrgId == orgid && segMeta.LatestEpochMS < oldest {
+			oldest = segMeta.LatestEpochMS
+		}
+	}
+
+	// Find the oldest metric meta
+	for _, metricMeta := range allMetricMetas {
+		metricMetaEpochMS := uint64(metricMeta.LatestEpochSec) * 1000 // convert to milliseconds
+		if metricMeta.OrgId == orgid && metricMetaEpochMS < oldest {
+			oldest = metricMetaEpochMS
+		}
+	}
+
+	return oldest, nil
+}
 func waitForInitialEvent() {
 	time.Sleep(2 * time.Minute)
 
@@ -210,6 +268,17 @@ func flushSsa() {
 	props.Set("runtime_os", runtime.GOOS)
 	props.Set("runtime_arch", runtime.GOARCH)
 	props.Set("id_source", source)
+	timestampFilePath := path.Join(config.GetDataPath(), "timestamp.txt")
+	data, err := os.ReadFile(timestampFilePath)
+	if err != nil {
+		log.Fatalf("Failed to read timestamp from file: %v", err)
+	}
+	timestamp, err := strconv.ParseInt(string(data), 10, 64)
+	if err != nil {
+		log.Fatalf("Failed to parse timestamp: %v", err)
+	}
+	days := int(time.Now().UnixMilli()-timestamp) / (60 * 60 * 24 * 1000)
+	props.Set("install_age", days)
 	_ = client.Enqueue(analytics.Track{
 		Event:      "server status",
 		UserId:     userId,
