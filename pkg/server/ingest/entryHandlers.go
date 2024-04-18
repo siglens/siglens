9ca529f9b0b98dd84f8ef62e823358fc49147b74
sunitakawane <skawane@sigscalr.io>
2024-04-18 12:40:59 +0530

    splunk ingest endpoint fix

--- pkg/server/ingest/entryHandlers.go
+++ pkg/server/ingest/entryHandlers.go
@@ -66,7 +66,7 @@ func getSafeHealthHandler() func(ctx *fasthttp.RequestCtx) {
 func splunkHecIngestHandler() func(ctx *fasthttp.RequestCtx) {
 	return func(ctx *fasthttp.RequestCtx) {
 		instrumentation.IncrementInt64Counter(instrumentation.POST_REQUESTS_COUNT, 1)
-		serverutils.CallWithOrgIdQuery(splunk.ProcessSplunkHecIngestRequest, ctx)
+		serverutils.CallWithOrgId(splunk.ProcessSplunkHecIngestRequest, ctx)
 	}
 }
 
@@ -78,13 +78,13 @@ func EsPutIndexHandler() func(ctx *fasthttp.RequestCtx) {
 
 func otsdbPutMetricsHandler() func(ctx *fasthttp.RequestCtx) {
 	return func(ctx *fasthttp.RequestCtx) {
-		serverutils.CallWithOrgIdQuery(otsdbwriter.PutMetrics, ctx)
+		serverutils.CallWithOrgId(otsdbwriter.PutMetrics, ctx)
 	}
 }
 
 func influxPutMetricsHandler() func(ctx *fasthttp.RequestCtx) {
 	return func(ctx *fasthttp.RequestCtx) {
-		serverutils.CallWithOrgIdQuery(influxwriter.PutMetrics, ctx)
+		serverutils.CallWithOrgId(influxwriter.PutMetrics, ctx)
 	}
 }
 
@@ -134,6 +134,6 @@ func getConfigReloadHandler() func(ctx *fasthttp.RequestCtx) {
 func lokiPostBulkHandler() func(ctx *fasthttp.RequestCtx) {
 	return func(ctx *fasthttp.RequestCtx) {
 		instrumentation.IncrementInt64Counter(instrumentation.POST_REQUESTS_COUNT, 1)
-		serverutils.CallWithOrgIdQuery(loki.ProcessLokiLogsIngestRequest, ctx)
+		serverutils.CallWithOrgId(loki.ProcessLokiLogsIngestRequest, ctx)
 	}
 }
