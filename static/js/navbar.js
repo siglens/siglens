e307264a51c164c58c1d6cda1a37d7eebb90783e
Sonam Gupta <71771131+sonamgupta21@users.noreply.github.com>
2024-05-13 19:45:47 +0530

    Fix: Dashboard and Metrics Explorer (#847)
    * Dashboard Setting Fix
    
    * Redirecting navbar icon to Metrics Explorer Page
    
    * Metrics Explorer - tag:value fix

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -33,7 +33,7 @@ let navbarComponent = `
                     class="nav-link-text">Tracing</span></a>
          </div>
         <div class="menu nav-metrics" title="Metrics">
-            <a href="./metrics.html" class="nav-links"><span class="icon-metrics"></span><span
+            <a href="./metrics-explorer.html" class="nav-links"><span class="icon-metrics"></span><span
                     class="nav-link-text">Metrics</span></a>
         </div>
         <div class="menu nav-slos" title="SLOs">
@@ -155,7 +155,7 @@ $(document).ready(function () {
 
     if (currentUrl.includes("index.html")) {
         $(".nav-search").addClass("active");
-    } else if (currentUrl.includes("metrics.html")) {
+    } else if (currentUrl.includes("metrics-explorer.html")) {
         $(".nav-metrics").addClass("active");
     } else if (currentUrl.includes("dashboards-home.html") || currentUrl.includes("dashboard.html")) {
         $(".nav-ldb").addClass("active");
