771dd5a97b54c76ae8c7a7cacac9a7e45dcaf58d
sonamgupta21 <sonam2001gupta@gmail.com>
2024-05-15 12:33:36 +0530

    Dashboards Index Dropdown

--- static/js/index.js
+++ static/js/index.js
@@ -61,3 +61,163 @@ function renderIndexDropdown(listIndices) {
         $("#index-btn span").html(sortedListIndices[0].index);
     }
 }
+
+// Dashboards Index Dropdown 
+// Function to initialize autocomplete with original index values
+async function initializeIndexAutocomplete() {
+    $("#index-listing").autocomplete({
+        source: indexValues,
+        minLength: 0,
+        select: function(event, ui) {
+            event.preventDefault();
+            const selectedValue = ui.item.value;
+            addSelectedIndex(selectedValue);
+
+
+            if (selectedValue.endsWith('*')) {
+                const prefix = selectedValue.slice(0, -1); // Remove the '*'
+                let filteredIndexValues = indexValues.filter(function(option) {
+                    return !option.startsWith(prefix);
+                });
+                indexValues = filteredIndexValues;
+                $(this).autocomplete("option", "source", filteredIndexValues);
+            }
+
+            const index = indexValues.indexOf(selectedValue);
+            if (index !== -1) {
+                indexValues.splice(index, 1);
+                $(this).autocomplete("option", "source", indexValues);
+            }
+            
+            // Update selectedSearchIndex
+            if (!selectedSearchIndex.includes(selectedValue)) {
+                selectedSearchIndex += selectedSearchIndex ? ',' + selectedValue : selectedValue;
+            }
+            
+            $(this).val('');
+            runQueryBtnHandler();
+        },
+        open: function(event, ui) {
+            var containerPosition = $(this).closest('.index-container').offset();
+    
+            $(this).autocomplete("widget").css({
+                "position": "absolute",
+                "top": containerPosition.top + $(this).closest('.index-container').outerHeight(),
+                "left": containerPosition.left,
+                "z-index": 1000
+            });
+        }
+    }).on('click', function() {
+        if ($(this).autocomplete('widget').is(':visible')) {
+            $(this).autocomplete('close');
+        } else {
+            $(this).autocomplete('search', '');
+        }
+    }).on('click', function() {
+        $(this).select();
+    }).on('input', function() {
+        let typedValue = $(this).val();
+        const minWidth = 3; 
+        const inputWidth = Math.max(typedValue.length * 8, minWidth);
+        this.style.width = inputWidth + 'px'; 
+    
+        // Check if the typed value matches any index exactly
+        const exactMatch = indexValues.includes(typedValue);
+        
+        // Filter out options with "*" from indexValues
+        const filteredIndexValues = indexValues.filter(function(option) {
+            return !option.includes('*');
+        });
+    
+        // If the typed value is empty or matches exactly, remove the "*" options
+        if (typedValue.trim() === '' || exactMatch) {
+            $(this).autocomplete("option", "source", filteredIndexValues);
+            return;
+        }
+    
+        // Check if the typed value matches any index
+        const matchesIndex = indexValues.some(function(option) {
+            return option.startsWith(typedValue);
+        });
+    
+        // If the typed value matches any index, add the option with "*"
+        if (matchesIndex && !typedValue.includes('*')) {
+            const wildcardOption = typedValue + '*'; // Create the option with "*"
+            filteredIndexValues.unshift(wildcardOption); // Add the option with "*" to the beginning of the array
+        }
+        $(this).autocomplete("option", "source", filteredIndexValues);
+    });
+}
+
+// Remove selected index from container when remove icon is clicked
+$(".index-container").on("click", ".remove-icon", function() {
+    if ($('.index-container .selected-index').length === 1) {
+        return; // If there's only one tag left, do not remove it
+    }
+
+    const removedValue = $(this).parent().contents().filter(function() {
+        return this.nodeType === 3;
+    }).text().trim();
+    $(this).parent().remove();
+
+    // If the removed value is a wildcard option
+    if (removedValue.endsWith('*')) {
+        const prefix = removedValue.slice(0, -1); // Remove the '*'
+        
+        // Remove wildcard options from indexValues
+        indexValues = indexValues.filter(function(option) {
+            return !option.startsWith(prefix);
+        });
+        
+        // Add back only the non-wildcard options that match the prefix
+        const matchingOptions = originalIndexValues.filter(function(option) {
+            return option.startsWith(prefix) && !selectedSearchIndex.includes(option);
+        });
+        
+        indexValues.push(...matchingOptions);
+        indexValues.sort();
+        $("#index-listing").autocomplete('option', 'source', indexValues);
+    } else {
+        // If the removed value is not a wildcard option
+        const prefix = removedValue;
+
+        // Check if any wildcard options exist that match the removed value
+        const wildcardExists = selectedSearchIndex.split(',').some(function(value) {
+            const strippedValue = value.replace('*', '');
+            return value.endsWith('*') && prefix.startsWith(strippedValue);
+        });
+
+        // If no wildcard options match the removed value's prefix, add it back to indexValues
+        if (!wildcardExists) {
+            indexValues.push(removedValue);
+            indexValues.sort();
+            $("#index-listing").autocomplete('option', 'source', indexValues);
+        }
+    }
+
+    // Update selectedSearchIndex
+    selectedSearchIndex = selectedSearchIndex.split(',').filter(function(value) {
+        return value !== removedValue;
+    }).join(',');
+
+    // Update the input width and placeholder if necessary
+    if ($('.index-container').find('.selected-index').length === 0) {
+        $("#index-listing").css('width', '100%');
+    }
+
+    runQueryBtnHandler();
+});
+
+
+function addSelectedIndex(index) {
+    const indexElement = $("<div class='selected-index'></div>").text(index);
+    const removeIcon = $("<span class='remove-icon'>×</span>");
+    indexElement.append(removeIcon);
+    $("#index-listing").before(indexElement);
+
+    if ($('.index-container').find('.selected-index').length === 0) {
+        $("#index-listing").css('width', '100%');
+    } else {
+        $("#index-listing").css('width', '5px');
+    }
+}
