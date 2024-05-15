0b0de0ae5ecef87446b9694cc7c9c04a690009f1
Sunita <skawane@sigscalr.io>
2024-05-15 19:28:59 +0530

    Use columnsOrder/allColumns field from backend to order columns (#865)
    * Use columnsOrder/allColumns field from backend to order columns
    
    * address PR comments
    
    * fix columnsOrder for 'logs-query'

--- static/js/ag-grid-seg-stats.js
+++ static/js/ag-grid-seg-stats.js
@@ -21,7 +21,7 @@
 
  let eGridDiv = null;
  
- function renderMeasuresGrid(columnOrder, hits) {
+ function renderMeasuresGrid(columnOrder, hits, groupByCols) {
      if (eGridDiv === null) {
          eGridDiv = document.querySelector('#measureAggGrid');
          new agGrid.Grid(eGridDiv, aggGridOptions);
@@ -46,8 +46,11 @@
      $.each(hits, function (key, resMap) {
         newRow.set("id", 0)
         columnOrder.map((colName, index) => {
-            if (resMap.GroupByValues!=null && resMap.GroupByValues[index]!="*" && index< (resMap.GroupByValues).length){
-                newRow.set(colName, resMap.GroupByValues[index])
+            let ind = -1;
+            if (groupByCols != undefined && groupByCols.length > 0)
+                ind = findColumnIndex(groupByCols, colName)
+            if (resMap.GroupByValues!=null && resMap.GroupByValues[ind]!="*" &&  ind< (resMap.GroupByValues).length && ind != -1){
+                newRow.set(colName, resMap.GroupByValues[ind])
             }else{
                 // Check if MeasureVal is undefined or null and set it to 0
                 if (resMap.MeasureVal[colName] === undefined || resMap.MeasureVal[colName] === null) {
@@ -55,7 +58,7 @@
                 } else {
                     newRow.set(colName, resMap.MeasureVal[colName]);
                 }
-            }
+            }            
         })
          segStatsRowData = _.concat(segStatsRowData, Object.fromEntries(newRow));
      })
@@ -71,3 +74,5 @@
     let metrics = context.measureText(text);
     return metrics.width;
   }
+
+  
