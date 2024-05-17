57f0704014a029e1fff23f0747120c528447ed97
sunitakawane <skawane@sigscalr.io>
2024-05-17 14:48:15 +0530

    added code formating changes as per jslint

--- static/js/ag-grid-seg-stats.js
+++ static/js/ag-grid-seg-stats.js
@@ -17,64 +17,71 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
- 'use strict';
 
- let eGridDiv = null;
- 
- function renderMeasuresGrid(columnOrder, hits) {
-     if (eGridDiv === null) {
-         eGridDiv = document.querySelector('#measureAggGrid');
-         new agGrid.Grid(eGridDiv, aggGridOptions);
-     }
-     // set the column headers from the data
-     let colDefs = aggGridOptions.api.getColumnDefs();
-     colDefs.length = 0;
-     colDefs = columnOrder.map((colName, index) => {
-         let title =  colName;
-         let resize = index + 1 == columnOrder.length ? false : true;
-         let maxWidth = Math.max(displayTextWidth(colName, "italic 19pt  DINpro "), 200)         //200 is approx width of 1trillion number
-         return {
-             field: title,
-             headerName: title,
-             resizable: resize,
-             minWidth: maxWidth,
-         };
-     });
-     aggsColumnDefs = _.chain(aggsColumnDefs).concat(colDefs).uniqBy('field').value();
-     aggGridOptions.api.setColumnDefs(aggsColumnDefs);
-     let newRow = new Map()
-     $.each(hits.measure, function (key, resMap) {
-        newRow.set("id", 0)
-        columnOrder.map((colName, index) => {
-            let ind=-1
-            if (hits.groupByCols !=undefined && hits.groupByCols.length > 0) {
-                ind = findColumnIndex(hits.groupByCols,colName)
+"use strict";
+
+let eGridDiv = null;
+
+function renderMeasuresGrid(columnOrder, hits) {
+    if (eGridDiv === null) {
+        eGridDiv = document.querySelector("#measureAggGrid");
+        new agGrid.Grid(eGridDiv, aggGridOptions);
+    }
+    // set the column headers from the data
+    let colDefs = aggGridOptions.api.getColumnDefs();
+    colDefs.length = 0;
+    colDefs = columnOrder.map(function (colName, index) {
+        let title = colName;
+        let resize = !(index + 1 === columnOrder.length) ? false : true;
+        let maxWidth = Math.max(displayTextWidth(
+            colName,
+            "italic 19pt  DINpro"
+        ),
+                200);
+        return {
+            field: title,
+            headerName: title,
+            resizable: resize,
+            minWidth: maxWidth
+        };
+    });
+    aggsColumnDefs = _.chain(aggsColumnDefs).concat(colDefs)
+        .uniqBy("field").value();
+    aggGridOptions.api.setColumnDefs(aggsColumnDefs);
+    let newRow = new Map();
+    $.each(hits.measure, function (_, resMap) {
+        newRow.set("id", 0);
+        columnOrder.map(function (colName) {
+            let ind = -1;
+            if (hits.groupByCols !== undefined && hits.groupByCols.length > 0) {
+                ind = findColumnIndex(hits.groupByCols, colName);
             }
             //group by col
-            if (ind !=-1  && resMap.GroupByValues.length != 1 && resMap.GroupByValues[ind]!="*"){
-                newRow.set(colName, resMap.GroupByValues[ind])
-            }else if (ind !=-1 && resMap.GroupByValues.length === 1 && resMap.GroupByValues[0]!="*"){
-                newRow.set(colName, resMap.GroupByValues[0])
-            }else{
+            if (ind !== -1 && resMap.GroupByValues.length !== 1
+                    && resMap.GroupByValues[ind] !== "*") {
+                newRow.set(colName, resMap.GroupByValues[ind]);
+            } else if (ind !== -1 && resMap.GroupByValues.length === 1
+                    && resMap.GroupByValues[0] !== "*") {
+                newRow.set(colName, resMap.GroupByValues[0]);
+            } else {
                 // Check if MeasureVal is undefined or null and set it to 0
-                if (resMap.MeasureVal[colName] === undefined || resMap.MeasureVal[colName] === null) {
+                if (resMap.MeasureVal[colName] === undefined ||
+                        resMap.MeasureVal[colName] === null) {
                     newRow.set(colName, "0");
                 } else {
                     newRow.set(colName, resMap.MeasureVal[colName]);
                 }
             }
-        })
-         segStatsRowData = _.concat(segStatsRowData, Object.fromEntries(newRow));
-     })
-     aggGridOptions.api.setRowData(segStatsRowData);
- 
- 
- }
- 
- function displayTextWidth(text, font) {
+        });
+        segStatsRowData = _.concat(segStatsRowData, Object.fromEntries(newRow));
+    });
+    aggGridOptions.api.setRowData(segStatsRowData);
+}
+
+function displayTextWidth(text, font) {
     let canvas = displayTextWidth.canvas || (displayTextWidth.canvas = document.createElement("canvas"));
     let context = canvas.getContext("2d");
     context.font = font;
     let metrics = context.measureText(text);
     return metrics.width;
-  }
+}
