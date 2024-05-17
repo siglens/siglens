e313ec082f1e0d5e3a7c9a65b1e4274bd57b2dec
tcefrep <1626680964@qq.com>
2024-05-17 10:14:37 -0400

    Add processing logic for promql funcs

--- pkg/segment/results/mresults/seriesresult.go
+++ pkg/segment/results/mresults/seriesresult.go
@@ -317,6 +317,10 @@ func ApplyMathFunction(ts map[uint32]float64, function structs.Function) (map[ui
 			return ts, fmt.Errorf("ApplyMathFunction: clamp_min has incorrect parameters: %v", function.ValueList)
 		}
 		evaluateClamp(ts, minVal, math.MaxFloat64)
+	case segutils.Timestamp:
+		for timestamp := range ts {
+			ts[timestamp] = float64(timestamp)
+		}
 	default:
 		return ts, fmt.Errorf("ApplyMathFunction: unsupported function type %v", function)
 	}
@@ -459,7 +463,7 @@ func ApplyRangeFunction(ts map[uint32]float64, function structs.Function) (map[u
 		delete(ts, sortedTimeSeries[0].downsampledTime)
 		return ts, nil
 	case segutils.IDelta:
-		// Calculate the instant delta for each timestamp, based on the last two data points within the timewindow
+		// Calculates the instant delta for each timestamp, based on the last two data points within the time window
 		for i := 1; i < len(sortedTimeSeries); i++ {
 			timeDff := sortedTimeSeries[i].downsampledTime - sortedTimeSeries[i-1].downsampledTime
 			if timeDff > timeWindow {
@@ -470,6 +474,65 @@ func ApplyRangeFunction(ts map[uint32]float64, function structs.Function) (map[u
 		}
 		// IDelta at left edge does not exist.
 		delete(ts, sortedTimeSeries[0].downsampledTime)
+		return ts, nil
+	case segutils.Changes:
+		// Calculates the number of times its value has changed within the provided time window
+		prefixSum := make([]float64, len(sortedTimeSeries))
+		prefixSum[0] = 0
+		for i := 1; i < len(sortedTimeSeries); i++ {
+			prefixSum[i] = prefixSum[i-1]
+			if sortedTimeSeries[i].dpVal != sortedTimeSeries[i-1].dpVal {
+				prefixSum[i]++
+			}
+		}
+
+		if len(ts) > 0 {
+			ts[sortedTimeSeries[0].downsampledTime] = 0
+		}
+
+		for i := 1; i < len(sortedTimeSeries); i++ {
+			timeWindowStartTime := sortedTimeSeries[i].downsampledTime - timeWindow
+			preIndex := sort.Search(len(sortedTimeSeries), func(j int) bool {
+				return sortedTimeSeries[j].downsampledTime >= timeWindowStartTime
+			})
+
+			if i <= preIndex { // Can not find the second point within the time window
+				ts[sortedTimeSeries[i].downsampledTime] = 0
+				continue
+			}
+
+			ts[sortedTimeSeries[i].downsampledTime] = prefixSum[i] - prefixSum[preIndex]
+		}
+		return ts, nil
+	case segutils.Resets:
+		// Any decrease in the value between two consecutive float samples is interpreted as a counter reset.
+		prefixSum := make([]float64, len(sortedTimeSeries))
+		prefixSum[0] = 0
+
+		for i := 1; i < len(sortedTimeSeries); i++ {
+			prefixSum[i] = prefixSum[i-1]
+			if sortedTimeSeries[i].dpVal < sortedTimeSeries[i-1].dpVal {
+				prefixSum[i]++
+			}
+		}
+
+		if len(ts) > 0 {
+			ts[sortedTimeSeries[0].downsampledTime] = 0
+		}
+
+		for i := 1; i < len(sortedTimeSeries); i++ {
+			timeWindowStartTime := sortedTimeSeries[i].downsampledTime - timeWindow
+			preIndex := sort.Search(len(sortedTimeSeries), func(j int) bool {
+				return sortedTimeSeries[j].downsampledTime >= timeWindowStartTime
+			})
+
+			if i <= preIndex { // Can not find the second point within the time window
+				ts[sortedTimeSeries[i].downsampledTime] = 0
+				continue
+			}
+
+			ts[sortedTimeSeries[i].downsampledTime] = prefixSum[i] - prefixSum[preIndex]
+		}
 		return ts, nil
 	default:
 		return ts, fmt.Errorf("ApplyRangeFunction: Unknown function type")
