44a178e8abc91f00998840264a22bab60286f938
tcefrep <1626680964@qq.com>
2024-05-30 09:57:13 -0400

    Fix arithmetic ops label matching logic between two vectors

--- pkg/integrations/prometheus/promql/metricsSearchHandler.go
+++ pkg/integrations/prometheus/promql/metricsSearchHandler.go
@@ -1287,56 +1287,6 @@ func ConvertPqlToMetricsQuery(searchText string, startTime, endTime uint32, myid
 	return []structs.MetricsQueryRequest{*metricQueryRequest}, pqlQuerytype, []structs.QueryArithmetic{}, nil
 }
 
-<<<<<<< HEAD
-=======
-func getLogicalAndArithmeticOperation(op parser.ItemType) segutils.LogicalAndArithmeticOperator {
-	switch op {
-	case parser.ADD:
-		return segutils.LetAdd
-	case parser.SUB:
-		return segutils.LetSubtract
-	case parser.MUL:
-		return segutils.LetMultiply
-	case parser.DIV:
-		return segutils.LetDivide
-	case parser.GTR:
-		return segutils.LetGreaterThan
-	case parser.GTE:
-		return segutils.LetGreaterThanOrEqualTo
-	case parser.LSS:
-		return segutils.LetLessThan
-	case parser.LTE:
-		return segutils.LetLessThanOrEqualTo
-	case parser.EQLC:
-		return segutils.LetEquals
-	case parser.NEQ:
-		return segutils.LetNotEquals
-	case parser.LAND:
-		return segutils.LetAnd
-	case parser.LOR:
-		return segutils.LetOr
-	case parser.LUNLESS:
-		return segutils.LetUnless
-	default:
-		log.Errorf("getArithmeticOperation: unexpected op: %v", op)
-		return 0
-	}
-}
-
-func isLogicalOperator(op parser.ItemType) bool {
-	switch op {
-	case parser.LAND:
-		return true
-	case parser.LOR:
-		return true
-	case parser.LUNLESS:
-		return true
-	default:
-		return false
-	}
-}
-
->>>>>>> 3278d28 (Add logical ops parsing logic)
 func parseTimeFromString(timeStr string) (uint32, error) {
 	// if it is not a relative time, parse as absolute time
 	var t time.Time
