1c6585671ec7dcab85b935b2759cd8883add16cd
Mani Chandra Teja Gaddam <manichandra.teja@gmail.com>
2024-06-06 11:31:26 -0400

    feat: regex search on metric name and group by metric name (#1049)

--- pkg/segment/structs/metricsstructs.go
+++ pkg/segment/structs/metricsstructs.go
@@ -36,16 +36,18 @@ import (
 Struct to represent a single metrics query request.
 */
 type MetricsQuery struct {
-	MetricName       string // metric name to query for.
-	QueryHash        uint64 // hash of the query
-	HashedMName      uint64
-	PqlQueryType     parser.ValueType // promql query type
-	Aggregator       Aggregation
-	Function         Function
-	Downsampler      Downsampler
-	TagsFilters      []*TagsFilter    // all tags filters to apply
-	TagIndicesToKeep map[int]struct{} // indices of tags to keep in the result
-	SelectAllSeries  bool             //flag to select all series - for promQl
+	MetricName             string            // metric name to query for.
+	MetricOperator         utils.TagOperator // operator to apply on metric name
+	MetricNameRegexPattern string            // regex pattern to apply on metric name
+	QueryHash              uint64            // hash of the query
+	HashedMName            uint64
+	PqlQueryType           parser.ValueType // promql query type
+	Aggregator             Aggregation
+	Function               Function
+	Downsampler            Downsampler
+	TagsFilters            []*TagsFilter    // all tags filters to apply
+	TagIndicesToKeep       map[int]struct{} // indices of tags to keep in the result
+	SelectAllSeries        bool             //flag to select all series - for promQl
 
 	MQueryAggs *MetricQueryAgg
 
@@ -58,6 +60,7 @@ type MetricsQuery struct {
 	TagValueSearchOnly  bool // flag to search only tag values
 	GetAllLabels        bool // flag to get all label sets for each time series
 	Groupby             bool // flag to group by tags
+	GroupByMetricName   bool // flag to group by metric name
 }
 
 type Aggregation struct {
@@ -243,6 +246,7 @@ type MetricsSearchRequest struct {
 	BlkWorkerParallelism uint
 	QueryType            SegType
 	AllTagKeys           map[string]bool
+	UnrotatedMetricNames map[string]bool
 }
 
 /*
@@ -437,4 +441,8 @@ func (metricFunc Function) ShallowClone() *Function {
 func (agg Aggregation) ShallowClone() *Aggregation {
 	aggCopy := agg
 	return &aggCopy
+}
+
+func (mQuery *MetricsQuery) IsRegexOnMetricName() bool {
+	return mQuery.MetricOperator == utils.Regex || mQuery.MetricOperator == utils.NegRegex
 }
