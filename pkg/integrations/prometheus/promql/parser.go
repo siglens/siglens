97527804bc32d1b4812e0d2c9b276ab8901dc820
tcefrep <1626680964@qq.com>
2024-06-06 09:44:40 -0400

    Fix up

--- pkg/integrations/prometheus/promql/parser.go
+++ pkg/integrations/prometheus/promql/parser.go
@@ -264,6 +264,7 @@ func handleAggregateExpr(expr *parser.AggregateExpr, mQuery *structs.MetricsQuer
 		mQuery.Aggregator.AggregatorFunction = segutils.Avg
 	case "count":
 		mQuery.Aggregator.AggregatorFunction = segutils.Count
+		mQuery.GetAllLabels = true
 	case "sum":
 		mQuery.Aggregator.AggregatorFunction = segutils.Sum
 	case "max":
