3e627c723298316e7bff6ebdec359bc0fac9879d
tcefrep <1626680964@qq.com>
2024-06-10 17:42:52 -0400

    Add todo comment

--- pkg/segment/aggregations/segaggs.go
+++ pkg/segment/aggregations/segaggs.go
@@ -121,6 +121,12 @@ func PostQueryBucketCleaning(nodeResult *structs.NodeResult, post *structs.Query
 	return nodeResult
 }
 
+/*
+* TODO: The processing logic for SPL commands that need to be implemented is outlined below. These commands may need to be implemented elsewhere.
+ 1. Stats cmd:
+    1.1 stats options: dedup_splitvals, allnum, partitions, delim
+    1.2 stats functions: estdc, estdc_error, exactperc99, perc66.6, median, stdev, stdevp, sumsq, upperperc6.6, var, varp, first, last, list, earliest, earliest_time, latest, latest_time, rate
+*/
 func performAggOnResult(nodeResult *structs.NodeResult, agg *structs.QueryAggregators, recs map[string]map[string]interface{},
 	recordIndexInFinal map[string]int, finalCols map[string]bool, numTotalSegments uint64, finishesSegment bool) error {
 	switch agg.PipeCommandType {
