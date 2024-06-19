e93d38cb5f07dba22f472b0a3aa03e235cd4d056
venkateshamatam <matam.v@northeastern.edu>
2024-06-19 16:54:11 -0400

    Add Test contact point API

--- pkg/server/query/server.go
+++ pkg/server/query/server.go
@@ -233,6 +233,7 @@ func (hs *queryserverCfg) Run(htmlTemplate *htmltemplate.Template, textTemplate 
 	hs.Router.DELETE(server_utils.API_PREFIX+"/alerts/deleteContact", hs.Recovery(deleteContactHandler()))
 	hs.Router.PUT(server_utils.API_PREFIX+"/alerts/silenceAlert", hs.Recovery(silenceAlertHandler()))
 
+	hs.Router.POST(server_utils.API_PREFIX+"/alerts/testContactPoint", hs.Recovery(testContactPointHandler()))
 	hs.Router.GET(server_utils.API_PREFIX+"/minionsearch/allMinionSearches", hs.Recovery(getAllMinionSearchesHandler()))
 	hs.Router.POST(server_utils.API_PREFIX+"/minionsearch/createMinionSearches", hs.Recovery(createMinionSearchHandler()))
 	hs.Router.GET(server_utils.API_PREFIX+"/minionsearch/{alertID}", hs.Recovery(getMinionSearchHandler()))
