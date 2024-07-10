c99dfa9e413af1c3f8afd406dce34f09bf3bf5ca
sunitakawane <sunitakawane@users.noreply.github.com>
2024-07-10 09:24:20 +0000

    github actions: style: format JS files

--- static/js/index.js
+++ static/js/index.js
@@ -17,7 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 let sortedListIndices;
 //eslint-disable-next-line no-unused-vars
 async function getListIndices() {
