672ce3a36dc90f37fd57fae30edfe3dbf4b8ab09
Divyanshu11011 <divyanshu1072.be21@chitkarauniversity.edu.in>
2024-07-19 20:17:50 +0530

    removed explicit index.html mention to save backend call

--- static/js/search.js
+++ static/js/search.js
@@ -784,7 +784,6 @@ function processCompleteUpdate(res, eventType, totalEventsSearched, timeToFirstB
     }
     isTimechart = res.isTimechart;
     const currentUrl = window.location.href;
-    if (currentUrl.includes('index.html')) timeChart();
     timeChart();
     let totalTime = new Date().getTime() - startQueryTime;
     let percentComplete = res.percent_complete;
