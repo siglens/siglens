e12992b19981e5d13453c0457db1ba0236e2863a
Dhruvi Trivedi <93305785+602dhruviii@users.noreply.github.com>
2024-08-07 17:14:22 +0530

    Update dashboard-charts.js h
    Signed-off-by: Dhruvi Trivedi <93305785+602dhruviii@users.noreply.github.com>

--- static/js/dashboard-charts.js
+++ static/js/dashboard-charts.js
@@ -565,6 +565,7 @@ function renderLineChart(seriesData, panelId) {
     if (panelId === -1) {
         panelChartEl.append(`<div class="lineChartLegend"></div>`);
     }
+    
     return lineChart;
 }
 
