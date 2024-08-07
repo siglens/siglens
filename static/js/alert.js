ef5b64b17e51e8e60459f6edede0e5d64c4057ea
Sonam Gupta <71771131+sonamgupta21@users.noreply.github.com>
2024-08-07 20:46:41 +0530

    UI Fixes (#1390)
    * Time Picker Should Not Update Dashboards
    
    * Remove Edit JSON Button from Default Dashboards & Disable Add Widget Popup on Save/Cancel
    
    * Remove Edit/Delete/Cancel from Alert History & Fix the Table for history and property
    
    * Time Picker Fix For Dashboards

--- static/js/alert.js
+++ static/js/alert.js
@@ -58,12 +58,15 @@ const propertiesGridOptions = {
         { headerName: 'Config Variable Value', field: 'value', sortable: true, filter: true, cellStyle: { 'white-space': 'normal', 'word-wrap': 'break-word' }, autoHeight: true },
     ],
     defaultColDef: {
+        cellClass: 'align-center-grid',
         resizable: true,
         flex: 1,
         minWidth: 150,
     },
     rowData: [],
     domLayout: 'autoHeight',
+    headerHeight: 32,
+    rowHeight: 42,
 };
 
 const historyGridOptions = {
@@ -73,12 +76,14 @@ const historyGridOptions = {
         { headerName: 'State', field: 'state', sortable: true, filter: true },
     ],
     defaultColDef: {
+        cellClass: 'align-center-grid',
         resizable: true,
         flex: 1,
         minWidth: 150,
     },
     rowData: [],
-    domLayout: 'autoHeight',
+    headerHeight: 32,
+    rowHeight: 42,
 };
 
 let originalIndexValues;
@@ -330,6 +335,7 @@ if (propertiesBtn) {
         propertiesBtn.classList.add('active');
         historyBtn.classList.remove('active');
         fetchAlertProperties();
+        $('#alert-details .btn-container').show();
     });
 }
 
@@ -341,6 +347,7 @@ if (historyBtn) {
         historyBtn.classList.add('active');
         propertiesBtn.classList.remove('active');
         displayHistoryData();
+        $('#alert-details .btn-container').hide();
     });
 }
 
