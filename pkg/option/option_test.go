daf8688aaa6b1318b13900a2086a8fc4a6ee9c42
Andrew Hess <ahess@sigscalr.io>
2024-08-11 17:37:38 -0600

    Refactor

--- pkg/option/option_test.go
+++ pkg/option/option_test.go
@@ -7,7 +7,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func verifyIsNil[U any](t *testing.T, nonNilValues []U, nilValues []U) {
+func Test_isNil(t *testing.T) {
+	// Non-nilable types.
+	verifyValidOrNil(t, []int{0, 42}, nil)
+	verifyValidOrNil(t, []string{"", "foo"}, nil)
+	verifyValidOrNil(t, []bool{false, true}, nil)
+	verifyValidOrNil(t, []float64{0.0, 42.0}, nil)
+
+	// Nilable types.
+	verifyValidOrNil(t, []map[string]int{{}, {"foo": 42}}, []map[string]int{nil})
+	verifyValidOrNil(t, [][]int{{}, {42}}, [][]int{nil})
+	verifyValidOrNil(t, []func() int{func() int { return 42 }}, []func() int{nil})
+	verifyValidOrNil(t, []func(x, y float32) float32{func(x, y float32) float32 { return 42 }}, []func(x, y float32) float32{nil})
+	verifyValidOrNil(t, []chan int{make(chan int)}, []chan int{nil})
+	verifyValidOrNil(t, []*int{new(int)}, []*int{nil})
+	verifyValidOrNil(t, []struct{ Value int }{{}, {42}}, []struct{ Value int }{})
+}
+
+func verifyValidOrNil[U any](t *testing.T, nonNilValues []U, nilValues []U) {
 	for _, value := range nonNilValues {
 		assert.False(t, isNil(value), fmt.Sprintf("value %v should not be nil", value))
 	}
@@ -15,23 +32,6 @@ func verifyIsNil[U any](t *testing.T, nonNilValues []U, nilValues []U) {
 	for _, value := range nilValues {
 		assert.True(t, isNil(value), fmt.Sprintf("value %v should be nil", value))
 	}
-}
-
-func Test_isNil(t *testing.T) {
-	// Non-nilable types.
-	verifyIsNil(t, []int{0, 42}, nil)
-	verifyIsNil(t, []string{"", "foo"}, nil)
-	verifyIsNil(t, []bool{false, true}, nil)
-	verifyIsNil(t, []float64{0.0, 42.0}, nil)
-
-	// Nilable types.
-	verifyIsNil(t, []map[string]int{{}, {"foo": 42}}, []map[string]int{nil})
-	verifyIsNil(t, [][]int{{}, {42}}, [][]int{nil})
-	verifyIsNil(t, []func() int{func() int { return 42 }}, []func() int{nil})
-	verifyIsNil(t, []func(x, y float32) float32{func(x, y float32) float32 { return 42 }}, []func(x, y float32) float32{nil})
-	verifyIsNil(t, []chan int{make(chan int)}, []chan int{nil})
-	verifyIsNil(t, []*int{new(int)}, []*int{nil})
-	verifyIsNil(t, []struct{ Value int }{{}, {42}}, []struct{ Value int }{})
 }
 
 func Test_Constructor_NonNilableType(t *testing.T) {
