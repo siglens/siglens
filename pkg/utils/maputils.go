ab5064bad78f633e18648d82215073964eb612e2
Andrew Hess <ahess@sigscalr.io>
2024-08-22 09:10:07 -0700

    Remove unused code

--- pkg/utils/maputils.go
+++ pkg/utils/maputils.go
@@ -116,12 +116,3 @@ func IntersectionWithFirstMapValues[K comparable, V1 any, V2 any](map1 map[K]V1,
 
 	return intersection
 }
-
-func ShallowCopyMap[K comparable, V any](m map[K]V) map[K]V {
-	mapCopy := make(map[K]V, len(m))
-	for k, v := range m {
-		mapCopy[k] = v
-	}
-
-	return mapCopy
-}
