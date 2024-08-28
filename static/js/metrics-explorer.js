943736c30f09aa6352d7f420f3e90dfd8cece6b2
dhruvi trivedi <dtrivedi@sigscalr.io>
2024-08-28 17:09:15 +0530

    resolved eslint

--- static/js/metrics-explorer.js
+++ static/js/metrics-explorer.js
@@ -2105,6 +2105,7 @@ async function getMetricsDataForFormula(formulaId, formulaDetails) {
     let queriesData = [];
     let formulas = [];
     let formulaString = formulaDetails.formula;
+    //eslint-disable-next-line no-unused-vars
     var canvas = $(`.metrics-graph[data-query="${formulaId}"] .graph-canvas`);
     var container = $('#metrics-graphs').find('.metrics-graph[data-query="' + formulaId + '"] .graph-canvas');
     container.append('<div id="panel-loading"></div>');
