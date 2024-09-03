751c1ce4e366358d4f449faef59a7446f22d89e2
vasukalariya <vasukalariya2@gmail.com>
2024-09-03 17:48:09 -0400

    release

--- cmd/startup/startup.go
+++ cmd/startup/startup.go
@@ -365,7 +365,7 @@ func startQueryServer(serverAddr string) {
 					return emptyHtmlContent
 				},
 				"CSSVersion": func() string {
-					return "0.2.32"
+					return "0.2.33"
 				},
 			})
 			textTemplate := texttemplate.New("other")
