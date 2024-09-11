ed46df0b20c6d722ae26530ffa373c5823aaf96a
fnu sathkrith <8sathkrithbhat@gmail.com>
2024-09-11 16:06:04 -0400

    move HasNumTypeAggForColumn

--- pkg/segment/structs/segstructs.go
+++ pkg/segment/structs/segstructs.go
@@ -1084,17 +1084,6 @@ func (qa *QueryAggregators) HasListFunc() bool {
 	return false
 }
 
-func HasNumTypeAggForColumn(measureOperations []*MeasureAggregator, column string) bool {
-	for _, measureOperation := range measureOperations {
-		if measureOperation.MeasureCol == column {
-			if utils.IsNumTypeAgg(measureOperation.MeasureFunc) {
-				return true
-			}
-		}
-	}
-	return false
-}
-
 func (qa *QueryAggregators) UsedByTimechart() bool {
 	return qa != nil && qa.TimeHistogram != nil && qa.TimeHistogram.Timechart != nil
 }
@@ -1581,4 +1570,15 @@ func StoreError(errorStore map[string]*SearchErrorInfo, errMsg string, logLevel 
 	}
 
 	return errorStore
+}
+
+func HasNumTypeAggForColumn(measureOperations []*MeasureAggregator, column string) bool {
+	for _, measureOperation := range measureOperations {
+		if measureOperation.MeasureCol == column {
+			if utils.IsNumTypeAgg(measureOperation.MeasureFunc) {
+				return true
+			}
+		}
+	}
+	return false
 }
