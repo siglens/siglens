618e62b864004be969bd13cb8b22a958b699a441
602dhruviii <602dhruviii@users.noreply.github.com>
2024-09-12 10:22:43 +0000

    github actions: style: format files

--- static/js/saved-query.js
+++ static/js/saved-query.js
@@ -349,11 +349,11 @@ function displaySavedQueries(res, flag) {
             newRow.set('searchText', value.searchText);
             newRow.set('indexName', value.indexName);
             newRow.set('type', res[key].dataSource);
-            if (res[key].dataSource === "metrics") {
+            if (res[key].dataSource === 'metrics') {
                 newRow.set('queryLanguage', 'PromQL');
             } else {
                 newRow.set('queryLanguage', res[key].queryLanguage);
-            } 
+            }
             newRow.set('qname', key);
             newRow.set('filterTab', value.filterTab);
             newRow.set('dataSource', value.dataSource);
@@ -381,11 +381,11 @@ function displaySavedQueries(res, flag) {
             newRow.set('searchText', value.searchText);
             newRow.set('indexName', value.indexName);
             newRow.set('qname', key);
-            if (value.dataSource === "metrics") {
+            if (value.dataSource === 'metrics') {
                 newRow.set('queryLanguage', 'PromQL');
             } else {
                 newRow.set('queryLanguage', value.queryLanguage);
-            }            
+            }
             newRow.set('filterTab', value.filterTab);
             newRow.set('type', value.dataSource);
             newRow.set('dataSource', value.dataSource);
