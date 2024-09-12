7cae2d21adb754e3cd0f8dcc7188ab10ef5adbb9
sonamgupta21 <sonam2001gupta@gmail.com>
2024-09-12 12:03:37 +0530

    Fix: Unable to Save Logs Query

--- static/js/metrics-explorer.js
+++ static/js/metrics-explorer.js
@@ -35,7 +35,7 @@ let timeUnit;
 let dayCnt7 = 0;
 let dayCnt2 = 0;
 // Used for alert screen
-let isAlertScreen, isMetricsURL, isDashboardScreen, isMetricsScreen;
+let isAlertScreen, isMetricsURL, isDashboardScreen;
 //eslint-disable-next-line no-unused-vars
 let metricsQueryParams;
 let funcApplied = false;
@@ -104,6 +104,7 @@ $(document).ready(async function () {
         isDashboardScreen = true;
     }
     if (currentPage === '/metrics-explorer.html') {
+        //eslint-disable-next-line no-undef
         isMetricsScreen = true;
     }
 
@@ -2087,6 +2088,7 @@ async function getMetricNames() {
 
 function displayErrorMessage(container, message) {
     var graphCanvas, errorSpan;
+    //eslint-disable-next-line no-undef
     if (isMetricsScreen) {
         const mergedContainer = document.querySelector('#merged-graph-container');
         graphCanvas = container.find('.graph-canvas');
