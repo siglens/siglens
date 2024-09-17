cd36c250f9c2151e4833c2e0e2920c92247d3a2f
Andrew Hess <ahess@sigscalr.io>
2024-09-17 15:43:01 -0600

    Revert a slice reversal change
    Our Go version is too old to use the new method I was using.

--- pkg/segment/query/queryrefresh.go
+++ pkg/segment/query/queryrefresh.go
@@ -21,7 +21,7 @@ import (
 	"bufio"
 	"errors"
 	"os"
-	"slices"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
@@ -231,16 +231,18 @@ func populateMicroIndices(smFile string) error {
 		return err
 	}
 
-	// Segmeta entries inside segmeta.json are added in increasing time order.
-	// we just reverse this and we get the latest segmeta entry first.
-	// This isn't required for correctness; it just avoids more sorting in the
-	// common case when we actually update the metadata.
-	slices.Reverse(allSegMetas)
-
 	allSmi := make([]*segmetadata.SegmentMicroIndex, len(allSegMetas))
 	for idx, segMetaInfo := range allSegMetas {
 		allSmi[idx] = segmetadata.ProcessSegmetaInfo(segMetaInfo)
 	}
+
+	// Segmeta entries inside segmeta.json are added in increasing time order.
+	// we just reverse this and we get the latest segmeta entry first.
+	// This isn't required for correctness; it just avoids more sorting in the
+	// common case when we actually update the metadata.
+	sort.SliceStable(allSmi, func(i, j int) bool {
+		return true
+	})
 
 	segmetadata.BulkAddSegmentMicroIndex(allSmi)
 	updateLastModifiedTimeForMetaFile(smFile, metaModificationTimeMs)
