a831a733dbe08fe152a444f786492c082f721e97
Macbeth98 <manichandra.teja@gmail.com>
2024-09-26 14:22:20 -0400

    refac the condition

--- pkg/segment/query/querystatus.go
+++ pkg/segment/query/querystatus.go
@@ -156,12 +156,10 @@ func StartQuery(qid uint64, async bool) (*RunningQueryState, error) {
 				rQuery, ok := allRunningQueries[qid]
 				arqMapLock.RUnlock()
 
-				if ok {
-					if ctx.Err() == context.DeadlineExceeded {
-						log.Infof("qid=%v Canceling query due to timeout (%v seconds)", qid, CANCEL_QUERY_AFTER_SECONDS)
-						rQuery.StateChan <- &QueryStateChanData{StateName: TIMEOUT}
-						CancelQuery(qid)
-					}
+				if ok && ctx.Err() == context.DeadlineExceeded {
+					log.Infof("qid=%v Canceling query due to timeout (%v seconds)", qid, CANCEL_QUERY_AFTER_SECONDS)
+					rQuery.StateChan <- &QueryStateChanData{StateName: TIMEOUT}
+					CancelQuery(qid)
 				}
 			}
 		}()
