0e8fad56fea56a64185840a377892dce1cd84add
Kunal Nawale <knawale@sigscalr.io>
2024-09-28 18:44:25 -0400

    reduce err log

--- pkg/segment/reader/segread/segreader.go
+++ pkg/segment/reader/segread/segreader.go
@@ -232,12 +232,11 @@ func (sfr *SegmentFileReader) ReadRecordFromBlock(blockNum uint16, recordNum uin
 		}
 	}
 
-	errStr := fmt.Sprintf("SegmentFileReader.ReadRecordFromBlock: reached end of block before matching recNum %+v, currRecordNum: %+v. blockNum %+v, File %+v, colname %v, sfr.currOffset: %v, sfr.currRecLen: %v, sfr.consistentColValueLen: %v",
+	errStr := fmt.Sprintf("SegmentFileReader.ReadRecordFromBlock: reached end of block before matching recNum %+v, currRecordNum: %+v. blockNum %+v, File %+v, colname %v, sfr.currOffset: %v, sfr.currRecLen: %v, sfr.currUncompressedBlockLen: %v",
 		recordNum, sfr.currRecordNum, blockNum, sfr.fileName, sfr.ColName, sfr.currOffset,
-		sfr.currRecLen, sfr.consistentColValueLen)
+		sfr.currRecLen, sfr.currUncompressedBlockLen)
 
 	log.Error(errStr)
-	log.Errorf("SegmentFileReader.ReadRecordFromBlock: Current offset %+v, blkLen: %+v", sfr.currOffset, sfr.currUncompressedBlockLen)
 	return nil, errors.New(errStr)
 }
 
