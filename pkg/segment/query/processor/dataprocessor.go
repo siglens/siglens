9a2f29d86a9722731c33ce5f5353c3c7d4805210
Andrew Hess <ahess@sigscalr.io>
2024-10-04 10:28:41 -0600

    Implement DataProcessor.Fetch()

--- pkg/segment/query/processor/dataprocessor.go
+++ pkg/segment/query/processor/dataprocessor.go
@@ -18,16 +18,22 @@
 package processor
 
 import (
+	"errors"
+	"fmt"
+	"io"
+
 	"github.com/siglens/siglens/pkg/segment/query/iqr"
+	"github.com/siglens/siglens/pkg/utils"
 )
 
 type streamer interface {
 	Fetch() (*iqr.IQR, error)
-	Reset()
+	Rewind()
 }
 
 type processor interface {
 	Process(*iqr.IQR) (*iqr.IQR, error)
+	Rewind()
 }
 
 type DataProcessor struct {
@@ -35,9 +41,10 @@ type DataProcessor struct {
 	processor processor
 
 	inputOrderMatters bool
-	isPermutingCmd    bool
-	isBottleneckCmd   bool
-	isTwoPassCmd      bool
+	isPermutingCmd    bool // This command may change the order of input.
+	isBottleneckCmd   bool // This command must see all input before yielding any output.
+	isTwoPassCmd      bool // A subset of bottleneck commands.
+	finishedFirstPass bool // Only used for two-pass commands.
 }
 
 func (dp *DataProcessor) DoesInputOrderMatter() bool {
@@ -54,4 +61,65 @@ func (dp *DataProcessor) IsBottleneckCmd() bool {
 
 func (dp *DataProcessor) IsTwoPassCmd() bool {
 	return dp.isTwoPassCmd
+}
+
+// Rewind sets up this DataProcessor to read the input streams from the
+// beginning; however, it doesn't fully reset it to its initial state. For
+// example, a two-pass command that finishes its first pass should remember
+// whatever state information it got from the first pass.
+func (dp *DataProcessor) Rewind() {
+	for _, stream := range dp.streams {
+		stream.Rewind()
+	}
+
+	dp.processor.Rewind()
+}
+
+func (dp *DataProcessor) Fetch() (*iqr.IQR, error) {
+	var output *iqr.IQR
+	gotEOF := false
+
+	for {
+		input, err := dp.getStreamInput()
+		if err == io.EOF {
+			gotEOF = true
+		} else if err != nil {
+			return nil, utils.TeeErrorf("DP.Fetch: failed to fetch input: %v", err)
+		}
+
+		output, err = dp.processor.Process(input)
+		if err != nil {
+			return nil, utils.TeeErrorf("DP.Fetch: failed to process input: %v", err)
+		}
+
+		if gotEOF {
+			if dp.isTwoPassCmd && !dp.finishedFirstPass {
+				dp.finishedFirstPass = true
+				dp.Rewind()
+				continue
+			}
+
+			if output == nil {
+				return output, io.EOF
+			} else {
+				return output, nil
+			}
+		} else if output != nil {
+			if !dp.isBottleneckCmd || (dp.isTwoPassCmd && dp.finishedFirstPass) {
+				return output, nil
+			}
+		}
+	}
+}
+
+func (dp *DataProcessor) getStreamInput() (*iqr.IQR, error) {
+	switch len(dp.streams) {
+	case 0:
+		return nil, errors.New("no streams")
+	case 1:
+		return dp.streams[0].Fetch()
+	}
+
+	// TODO: fetch from all streams and merge them.
+	return nil, fmt.Errorf("not implemented")
 }
