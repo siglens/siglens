8ee100f7eacfbf553937ed37672fdb426d946c16
sonamgupta21 <sonamgupta21@users.noreply.github.com>
2024-10-10 07:17:56 +0000

    github actions: style: format files

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1423,7 +1423,6 @@ function setDashboardQueryModeHandler(panelQueryMode) {
     }
 }
 
-
 $('#run-dashboard-fliter').on('click', function () {
     const filterValue = $('.search-db-input').val();
     if (!validateFilterInput(filterValue)) {
