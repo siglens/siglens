0909fd6b8a7b94d6831966d265d10b90ede8172d
Andrew Hess <ahess@sigscalr.io>
2024-10-18 15:32:14 -0600

    Handle dedup with consecutive=true

--- pkg/segment/query/processor/dedupcommand.go
+++ pkg/segment/query/processor/dedupcommand.go
@@ -18,22 +18,135 @@
 package processor
 
 import (
+	"io"
+
 	"github.com/siglens/siglens/pkg/segment/query/iqr"
 	"github.com/siglens/siglens/pkg/segment/structs"
+	segutils "github.com/siglens/siglens/pkg/segment/utils"
+	toputils "github.com/siglens/siglens/pkg/utils"
+	log "github.com/sirupsen/logrus"
 )
 
 type dedupProcessor struct {
-	options *structs.DedupExpr
+	options         *structs.DedupExpr
+	prevCombination []*segutils.CValueEnclosure
 }
 
 func (p *dedupProcessor) Process(iqr *iqr.IQR) (*iqr.IQR, error) {
-	panic("not implemented")
+	if len(p.options.DedupSortEles) > 0 {
+		return nil, toputils.TeeErrorf("dedup.Process: sorting is not yet implemented")
+	}
+
+	if len(p.options.FieldList) == 0 {
+		return nil, toputils.TeeErrorf("dedup.Process: no field specified")
+	}
+
+	if iqr == nil {
+		return nil, io.EOF
+	}
+
+	if iqr.NumberOfRecords() == 0 {
+		return iqr, nil
+	}
+
+	fieldToValues := make(map[string][]segutils.CValueEnclosure)
+	for _, field := range p.options.FieldList {
+		values, err := iqr.ReadColumn(field)
+		if err != nil {
+			if p.options.DedupOptions.KeepEmpty {
+				return iqr, nil
+			} else {
+				// Drop all rows.
+				return nil, nil
+			}
+		}
+
+		fieldToValues[field] = values
+	}
+
+	rowsToDiscard := make([]int, 0)
+
+	if p.options.DedupOptions.Consecutive {
+		numRecords := len(fieldToValues[p.options.FieldList[0]])
+		curCombination := make([]*segutils.CValueEnclosure, len(p.options.FieldList))
+
+		startIndex := 0
+		if p.prevCombination == nil {
+			startIndex = 1
+			p.prevCombination = make([]*segutils.CValueEnclosure, len(p.options.FieldList))
+			for i, field := range p.options.FieldList {
+				p.prevCombination[i] = &fieldToValues[field][0]
+			}
+		}
+
+	RecordLoop:
+		for i := startIndex; i < numRecords; i++ {
+			for k, field := range p.options.FieldList {
+				curCombination[k] = &fieldToValues[field][i]
+
+				if fieldToValues[field][i].Dtype == segutils.SS_DT_BACKFILL {
+					if !p.options.DedupOptions.KeepEmpty {
+						rowsToDiscard = append(rowsToDiscard, i)
+					}
+
+					copy(p.prevCombination, curCombination)
+					continue RecordLoop
+				}
+			}
+
+			if shouldDiscardConsecutive(p.prevCombination, curCombination) {
+				rowsToDiscard = append(rowsToDiscard, i)
+			}
+
+			copy(p.prevCombination, curCombination)
+		}
+	}
+
+	// Discard the records.
+	if p.options.DedupOptions.KeepEvents {
+		// Clear the fields instead of discarding the rows.
+		for _, values := range fieldToValues {
+			for _, rowIndex := range rowsToDiscard {
+				values[rowIndex].Dtype = segutils.SS_DT_BACKFILL
+				values[rowIndex].CVal = nil
+			}
+		}
+	} else {
+		err := iqr.DiscardRows(rowsToDiscard)
+		if err != nil {
+			log.Errorf("dedup.Process: failed to discard rows: %v", err)
+			return nil, err
+		}
+	}
+
+	return iqr, nil
 }
 
 func (p *dedupProcessor) Rewind() {
-	panic("not implemented")
+	p.prevCombination = nil
 }
 
 func (p *dedupProcessor) Cleanup() {
-	panic("not implemented")
+	p.prevCombination = nil
+}
+
+func shouldDiscardConsecutive(prevCombination, curCombination []*segutils.CValueEnclosure) bool {
+	if len(prevCombination) == 0 {
+		// The curCombination is the first record.
+		return false
+	}
+
+	if len(prevCombination) != len(curCombination) {
+		log.Errorf("shouldDiscardConsecutive: combinations have different lengths (%v and %v)",
+			len(prevCombination), len(curCombination))
+		return false
+	}
+
+	for i := 0; i < len(prevCombination); i++ {
+		if *prevCombination[i] != *curCombination[i] {
+			return false
+		}
+	}
+
+	return true
 }
---
+++ pkg/segment/query/processor/dedupcommand_test.go
@@ -0,0 +1,177 @@
+// Copyright (c) 2021-2024 SigScalr, Inc.
+//
+// This file is part of SigLens Observability Solution
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package processor
+
+import (
+	"testing"
+
+	"github.com/siglens/siglens/pkg/segment/query/iqr"
+	"github.com/siglens/siglens/pkg/segment/structs"
+	"github.com/siglens/siglens/pkg/segment/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func makeDedup(t *testing.T, limit uint64, fieldList []string, consecutive bool,
+	keepEmpty bool, keepEvents bool) *DataProcessor {
+
+	options := &structs.DedupExpr{
+		Limit:     limit,
+		FieldList: fieldList,
+		DedupOptions: &structs.DedupOptions{
+			KeepEmpty:   keepEmpty,
+			Consecutive: consecutive,
+			KeepEvents:  keepEvents,
+		},
+	}
+
+	dataProcessor := NewDedupDP(options)
+	assert.NotNil(t, dataProcessor)
+	return dataProcessor
+}
+
+func Test_Dedup_consecutive(t *testing.T) {
+	dataProcessor := makeDedup(t, 1, []string{"col1"}, true, false, false)
+
+	iqr1 := iqr.NewIQR(0)
+	err := iqr1.AppendKnownValues(map[string][]utils.CValueEnclosure{
+		"col1": {
+			{Dtype: utils.SS_DT_STRING, CVal: "a"},
+			{Dtype: utils.SS_DT_STRING, CVal: "a"},
+			{Dtype: utils.SS_DT_STRING, CVal: "b"},
+		},
+	})
+	assert.NoError(t, err)
+
+	result1, err := dataProcessor.processor.Process(iqr1)
+	assert.NoError(t, err)
+	assert.NotNil(t, result1)
+
+	expected := []utils.CValueEnclosure{
+		{Dtype: utils.SS_DT_STRING, CVal: "a"},
+		{Dtype: utils.SS_DT_STRING, CVal: "b"},
+	}
+	actual, err := result1.ReadColumn("col1")
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+
+	iqr2 := iqr.NewIQR(0)
+	err = iqr2.AppendKnownValues(map[string][]utils.CValueEnclosure{
+		"col1": {
+			{Dtype: utils.SS_DT_STRING, CVal: "b"},
+			{Dtype: utils.SS_DT_STRING, CVal: "c"},
+			{Dtype: utils.SS_DT_STRING, CVal: "c"},
+		},
+	})
+	assert.NoError(t, err)
+
+	result2, err := dataProcessor.processor.Process(iqr2)
+	assert.NoError(t, err)
+	assert.NotNil(t, result2)
+
+	// The last batch ended with a "b", so the "b" at the start of this batch
+	// should be dropped.
+	expected = []utils.CValueEnclosure{
+		{Dtype: utils.SS_DT_STRING, CVal: "c"},
+	}
+	actual, err = result2.ReadColumn("col1")
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func Test_Dedup_keepEmpty(t *testing.T) {
+	dataProcessor := makeDedup(t, 1, []string{"col1"}, true, true, false)
+
+	iqr1 := iqr.NewIQR(0)
+	err := iqr1.AppendKnownValues(map[string][]utils.CValueEnclosure{
+		"col1": {
+			{Dtype: utils.SS_DT_STRING, CVal: "a"},
+			{Dtype: utils.SS_DT_BACKFILL, CVal: nil},
+			{Dtype: utils.SS_DT_BACKFILL, CVal: nil},
+		},
+	})
+	assert.NoError(t, err)
+
+	result1, err := dataProcessor.processor.Process(iqr1)
+	assert.NoError(t, err)
+	assert.NotNil(t, result1)
+
+	expected := []utils.CValueEnclosure{
+		{Dtype: utils.SS_DT_STRING, CVal: "a"},
+		{Dtype: utils.SS_DT_BACKFILL, CVal: nil},
+		{Dtype: utils.SS_DT_BACKFILL, CVal: nil},
+	}
+	actual, err := result1.ReadColumn("col1")
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func Test_Dedup_dropEmpty(t *testing.T) {
+	dataProcessor := makeDedup(t, 1, []string{"col1"}, true, false, false)
+
+	iqr1 := iqr.NewIQR(0)
+	err := iqr1.AppendKnownValues(map[string][]utils.CValueEnclosure{
+		"col1": {
+			{Dtype: utils.SS_DT_STRING, CVal: "a"},
+			{Dtype: utils.SS_DT_BACKFILL, CVal: nil},
+			{Dtype: utils.SS_DT_BACKFILL, CVal: nil},
+		},
+	})
+	assert.NoError(t, err)
+
+	result1, err := dataProcessor.processor.Process(iqr1)
+	assert.NoError(t, err)
+	assert.NotNil(t, result1)
+
+	expected := []utils.CValueEnclosure{
+		{Dtype: utils.SS_DT_STRING, CVal: "a"},
+	}
+	actual, err := result1.ReadColumn("col1")
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func Test_Dedup_keepEvents(t *testing.T) {
+	dataProcessor := makeDedup(t, 1, []string{"col1"}, true, false, true)
+
+	iqr1 := iqr.NewIQR(0)
+	err := iqr1.AppendKnownValues(map[string][]utils.CValueEnclosure{
+		"col1": {
+			{Dtype: utils.SS_DT_STRING, CVal: "a"},
+			{Dtype: utils.SS_DT_STRING, CVal: "a"},
+			{Dtype: utils.SS_DT_STRING, CVal: "b"},
+			{Dtype: utils.SS_DT_STRING, CVal: "a"},
+			{Dtype: utils.SS_DT_STRING, CVal: "a"},
+		},
+	})
+	assert.NoError(t, err)
+
+	result1, err := dataProcessor.processor.Process(iqr1)
+	assert.NoError(t, err)
+	assert.NotNil(t, result1)
+
+	expected := []utils.CValueEnclosure{
+		{Dtype: utils.SS_DT_STRING, CVal: "a"},
+		{Dtype: utils.SS_DT_BACKFILL, CVal: nil},
+		{Dtype: utils.SS_DT_STRING, CVal: "b"},
+		{Dtype: utils.SS_DT_STRING, CVal: "a"},
+		{Dtype: utils.SS_DT_BACKFILL, CVal: nil},
+	}
+	actual, err := result1.ReadColumn("col1")
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
