ca0e937625739ccfd21b03d2377fb7d0a7a8ad6d
Andrew Hess <ahess@sigscalr.io>
2024-10-19 06:07:40 -0700

    Add bin with span to new query pipeline (#1767)
    * Override RRC values with knownValues
    
    * Handle bin when span is specified
    
    * Include bin field renaming in the bin options
    
    * Handle renaming the bin field
    
    * Discard renamed columns read from RRCs

--- pkg/segment/query/iqr/intermediateQueryResult.go
+++ pkg/segment/query/iqr/intermediateQueryResult.go
@@ -228,10 +228,6 @@ func (iqr *IQR) ReadColumn(cname string) ([]utils.CValueEnclosure, error) {
 		return nil, fmt.Errorf("IQR.ReadColumn: column %s is deleted", cname)
 	}
 
-	if newCname, ok := iqr.renamedColumns[cname]; ok {
-		cname = newCname
-	}
-
 	if values, ok := iqr.knownValues[cname]; ok {
 		return values, nil
 	}
@@ -285,6 +281,16 @@ func (iqr *IQR) readAllColumnsWithRRCs() (map[string][]utils.CValueEnclosure, er
 			return nil, toputils.TeeErrorf("IQR.readAllColumnsWithRRCs: expected %v results, got %v",
 				len(iqr.rrcs), len(values))
 		}
+	}
+
+	for oldName := range iqr.renamedColumns {
+		// TODO: don't read these columns from the RRCs, instead of reading and
+		// then deleting them.
+		delete(results, oldName)
+	}
+
+	for cname, values := range iqr.knownValues {
+		results[cname] = values
 	}
 
 	return results, nil
@@ -655,6 +661,21 @@ func (iqr *IQR) DiscardRows(rowsToDiscard []int) error {
 		}
 
 		iqr.knownValues[cname] = newValues
+	}
+
+	return nil
+}
+
+func (iqr *IQR) RenameColumn(oldName, newName string) error {
+	if err := iqr.validate(); err != nil {
+		log.Errorf("IQR.RenameColumn: validation failed: %v", err)
+		return err
+	}
+
+	iqr.renamedColumns[oldName] = newName
+	if values, ok := iqr.knownValues[oldName]; ok {
+		iqr.knownValues[newName] = values
+		delete(iqr.knownValues, oldName)
 	}
 
 	return nil
