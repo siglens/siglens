f6c710e991aebb0a2027dd4118c0c6271d5a392c
Macbeth98 <manichandra.teja@gmail.com>
2024-10-21 14:50:34 -0400

    revamped the logic of fetching of Allcolumns

--- pkg/segment/query/iqr/intermediateQueryResult.go
+++ pkg/segment/query/iqr/intermediateQueryResult.go
@@ -395,21 +395,42 @@ func (iqr *IQR) Append(other *IQR) error {
 	return nil
 }
 
+func (iqr *IQR) getSegKeyToVirtualTableMapFromRRCs() map[string]string {
+	// segKey -> virtual table name
+	segKeyVTableMap := make(map[string]string)
+	for _, rrc := range iqr.rrcs {
+		segKey, ok := iqr.encodingToSegKey[rrc.SegKeyInfo.SegKeyEnc]
+		if !ok {
+			// This should never happen.
+			log.Errorf("qid=%v, IQR.getSegKeysFromRRCs: unknown encoding %v", iqr.qid, rrc.SegKeyInfo.SegKeyEnc)
+			continue
+		}
+
+		if _, ok := segKeyVTableMap[segKey]; ok {
+			continue
+		}
+		segKeyVTableMap[segKey] = rrc.VirtualTableName
+	}
+
+	return segKeyVTableMap
+}
+
 func (iqr *IQR) GetColumns() (map[string]struct{}, error) {
 	if err := iqr.validate(); err != nil {
 		return nil, toputils.TeeErrorf("IQR.GetColumns: validation failed: %v", err)
 	}
 
-	segKey, ok := iqr.encodingToSegKey[iqr.rrcs[0].SegKeyInfo.SegKeyEnc]
-	if !ok {
-		return nil, toputils.TeeErrorf("IQR.readColumnWithRRCs: unknown encoding %v", iqr.rrcs[0].SegKeyInfo.SegKeyEnc)
-	}
+	segKeyVTableMap := iqr.getSegKeyToVirtualTableMapFromRRCs()
 
-	vTable := iqr.rrcs[0].VirtualTableName
+	allColumns := make(map[string]struct{})
 
-	allColumns, err := record.GetColsForSegKey(segKey, vTable)
-	if err != nil {
-		return nil, err
+	for segkey, vTable := range segKeyVTableMap {
+		columns, err := record.GetColsForSegKey(segkey, vTable)
+		if err != nil {
+			log.Errorf("qid=%v, IQR.GetColumns: error getting columns for segKey %v: %v and Virtual Table name: %v", iqr.qid, segkey, vTable, err)
+		}
+
+		toputils.AddMapKeysToSet(allColumns, columns)
 	}
 
 	toputils.AddMapKeysToSet(allColumns, iqr.knownValues)
