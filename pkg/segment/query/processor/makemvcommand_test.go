b10436372661274d2063d169407a84d615564b27
Andrew Hess <ahess@sigscalr.io>
2024-10-31 15:22:25 -0600

    Add more unit tests

--- pkg/segment/query/processor/makemvcommand_test.go
+++ pkg/segment/query/processor/makemvcommand_test.go
@@ -34,7 +34,7 @@ func Test_MakeMV_simple(t *testing.T) {
 			DelimiterString: ",",
 			IsRegex:         false,
 			AllowEmpty:      false,
-			Limit:           10,
+			Setsv:           false,
 		},
 	}
 	iqr := iqr.NewIQR(0)
@@ -44,6 +44,7 @@ func Test_MakeMV_simple(t *testing.T) {
 			{Dtype: utils.SS_DT_STRING, CVal: "d,e,f"},
 		},
 	})
+	assert.NoError(t, err)
 
 	iqr, err = makemv.Process(iqr)
 	assert.NoError(t, err)
@@ -51,6 +52,174 @@ func Test_MakeMV_simple(t *testing.T) {
 	expected := []utils.CValueEnclosure{
 		{Dtype: utils.SS_DT_STRING_SLICE, CVal: []string{"a", "b", "c"}},
 		{Dtype: utils.SS_DT_STRING_SLICE, CVal: []string{"d", "e", "f"}},
+	}
+
+	actual, err := iqr.ReadColumn("col1")
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func Test_MakeMV_multicharDelimeter(t *testing.T) {
+	makemv := &makemvProcessor{
+		options: &structs.MultiValueColLetRequest{
+			Command:         "makemv",
+			ColName:         "col1",
+			DelimiterString: "|foo|",
+			IsRegex:         false,
+			AllowEmpty:      false,
+			Setsv:           false,
+		},
+	}
+
+	iqr := iqr.NewIQR(0)
+	err := iqr.AppendKnownValues(map[string][]utils.CValueEnclosure{
+		"col1": {
+			{Dtype: utils.SS_DT_STRING, CVal: "a|foo|b|foo|c"},
+			{Dtype: utils.SS_DT_STRING, CVal: "d|foo|e|foo|f"},
+		},
+	})
+	assert.NoError(t, err)
+
+	iqr, err = makemv.Process(iqr)
+	assert.NoError(t, err)
+
+	expected := []utils.CValueEnclosure{
+		{Dtype: utils.SS_DT_STRING_SLICE, CVal: []string{"a", "b", "c"}},
+		{Dtype: utils.SS_DT_STRING_SLICE, CVal: []string{"d", "e", "f"}},
+	}
+
+	actual, err := iqr.ReadColumn("col1")
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func Test_MakeMV_overlappingDelimeter(t *testing.T) {
+	makemv := &makemvProcessor{
+		options: &structs.MultiValueColLetRequest{
+			Command:         "makemv",
+			ColName:         "col1",
+			DelimiterString: "||",
+			IsRegex:         false,
+			AllowEmpty:      false,
+			Setsv:           false,
+		},
+	}
+
+	iqr := iqr.NewIQR(0)
+	err := iqr.AppendKnownValues(map[string][]utils.CValueEnclosure{
+		"col1": {
+			{Dtype: utils.SS_DT_STRING, CVal: "a|||b||||c"}, // Three and four consecutive pipes
+		},
+	})
+	assert.NoError(t, err)
+
+	iqr, err = makemv.Process(iqr)
+	assert.NoError(t, err)
+
+	expected := []utils.CValueEnclosure{
+		{Dtype: utils.SS_DT_STRING_SLICE, CVal: []string{"a", "|b", "c"}},
+	}
+
+	actual, err := iqr.ReadColumn("col1")
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func Test_MakeMV_allowEmpty(t *testing.T) {
+	makemv := &makemvProcessor{
+		options: &structs.MultiValueColLetRequest{
+			Command:         "makemv",
+			ColName:         "col1",
+			DelimiterString: ",",
+			IsRegex:         false,
+			AllowEmpty:      true,
+			Setsv:           false,
+		},
+	}
+
+	iqr := iqr.NewIQR(0)
+	err := iqr.AppendKnownValues(map[string][]utils.CValueEnclosure{
+		"col1": {
+			{Dtype: utils.SS_DT_STRING, CVal: "a,,b,"},
+			{Dtype: utils.SS_DT_STRING, CVal: ",c,d,e"},
+		},
+	})
+	assert.NoError(t, err)
+
+	iqr, err = makemv.Process(iqr)
+	assert.NoError(t, err)
+
+	expected := []utils.CValueEnclosure{
+		{Dtype: utils.SS_DT_STRING_SLICE, CVal: []string{"a", "", "b", ""}},
+		{Dtype: utils.SS_DT_STRING_SLICE, CVal: []string{"", "c", "d", "e"}},
+	}
+
+	actual, err := iqr.ReadColumn("col1")
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func Test_MakeMV_regex(t *testing.T) {
+	makemv := &makemvProcessor{
+		options: &structs.MultiValueColLetRequest{
+			Command:         "makemv",
+			ColName:         "col1",
+			DelimiterString: `([a-zA-Z]+)`,
+			IsRegex:         true,
+			AllowEmpty:      true,
+			Setsv:           false,
+		},
+	}
+
+	iqr := iqr.NewIQR(0)
+	err := iqr.AppendKnownValues(map[string][]utils.CValueEnclosure{
+		"col1": {
+			{Dtype: utils.SS_DT_STRING, CVal: "123abc456def"},
+			{Dtype: utils.SS_DT_STRING, CVal: "99XYZ88ABC"},
+		},
+	})
+	assert.NoError(t, err)
+
+	iqr, err = makemv.Process(iqr)
+	assert.NoError(t, err)
+
+	expected := []utils.CValueEnclosure{
+		{Dtype: utils.SS_DT_STRING_SLICE, CVal: []string{"abc", "def"}},
+		{Dtype: utils.SS_DT_STRING_SLICE, CVal: []string{"XYZ", "ABC"}},
+	}
+
+	actual, err := iqr.ReadColumn("col1")
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func Test_MakeMV_setSV(t *testing.T) {
+	makemv := &makemvProcessor{
+		options: &structs.MultiValueColLetRequest{
+			Command:         "makemv",
+			ColName:         "col1",
+			DelimiterString: ",",
+			IsRegex:         false,
+			AllowEmpty:      false,
+			Setsv:           true,
+		},
+	}
+
+	iqr := iqr.NewIQR(0)
+	err := iqr.AppendKnownValues(map[string][]utils.CValueEnclosure{
+		"col1": {
+			{Dtype: utils.SS_DT_STRING, CVal: "apple,banana,cherry"},
+			{Dtype: utils.SS_DT_STRING, CVal: "dog,cat,fish"},
+		},
+	})
+	assert.NoError(t, err)
+
+	iqr, err = makemv.Process(iqr)
+	assert.NoError(t, err)
+
+	expected := []utils.CValueEnclosure{
+		{Dtype: utils.SS_DT_STRING, CVal: "apple banana cherry"},
+		{Dtype: utils.SS_DT_STRING, CVal: "dog cat fish"},
 	}
 
 	actual, err := iqr.ReadColumn("col1")
