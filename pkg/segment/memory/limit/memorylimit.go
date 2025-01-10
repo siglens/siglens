97cdc7bd1f77f97e2d2860127ccfb16640c419a3
Mani Chandra Teja Gaddam <manichandra.teja@gmail.com>
2025-01-09 21:23:14 -0500

    Fix: Reset segment search memory to Initial Allocated Value during rebalance (#2040)
    * Reset segment search memory when it is lowered from the initialized value
    
    * refac: variable names
    
    ---------
    
    Co-authored-by: Kunal Nawale <knawale@sigscalr.io>

--- pkg/segment/memory/limit/memorylimit.go
+++ pkg/segment/memory/limit/memorylimit.go
@@ -33,6 +33,8 @@ import (
 
 const MINUTES_UPDATE_METADATA_MEM_ALLOC = 1
 
+var SegSearchAllocatedBytes uint64 // Should not be changed after initialization
+
 var LOG_GLOBAL_MEM_FREQUENCY = 5
 
 func InitMemoryLimiter() {
@@ -41,7 +43,7 @@ func InitMemoryLimiter() {
 
 	memLimits := config.GetMemoryConfig()
 
-	segSearchBytes := uint64(float64(totalAvailableSizeBytes*memLimits.SearchPercent) / 100)
+	SegSearchAllocatedBytes = uint64(float64(totalAvailableSizeBytes*memLimits.SearchPercent) / 100)
 	rotatedCMIBytes := uint64(float64(totalAvailableSizeBytes*memLimits.CMIPercent) / 100)
 	metricsInMemory := uint64(float64(totalAvailableSizeBytes*memLimits.MetricsPercent) / 100)
 
@@ -52,7 +54,7 @@ func InitMemoryLimiter() {
 	memory.GlobalMemoryTracker = &structs.MemoryTracker{
 		TotalAllocatableBytes:   totalAvailableSizeBytes,
 		RotatedCMIBytesInMemory: rotatedCMIBytes,
-		SegSearchRequestedBytes: segSearchBytes,
+		SegSearchRequestedBytes: SegSearchAllocatedBytes,
 		MetricsSegmentMaxSize:   metricsInMemory,
 
 		SegWriterUsageBytes: 0,
@@ -132,6 +134,11 @@ func rebalanceMemoryAllocation() {
 	memLimits := config.GetMemoryConfig()
 	totalSsmMemory := uint64(float64(memoryAvailable*memLimits.MetadataPercent) / 100)
 	segmetadata.RebalanceInMemorySsm(totalSsmMemory)
+
+	if memory.GlobalMemoryTracker.SegSearchRequestedBytes < SegSearchAllocatedBytes {
+		// reset the allocatedSegSearchBytes as we may have freed up memory
+		atomic.StoreUint64(&memory.GlobalMemoryTracker.SegSearchRequestedBytes, SegSearchAllocatedBytes)
+	}
 
 	if memory.GlobalMemoryTracker.SegSearchRequestedBytes > memoryAvailable {
 		memoryAvailable = 0
