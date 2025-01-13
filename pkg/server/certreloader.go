18fa0b19d3cd0082c6e4e8e750ad959b226e487d
Andrew Hess <ahess@sigscalr.io>
2025-01-13 10:49:21 -0700

    Update logging

--- pkg/server/certreloader.go
+++ pkg/server/certreloader.go
@@ -2,7 +2,10 @@ package server
 
 import (
 	"crypto/tls"
+	"crypto/x509"
+	"fmt"
 	"sync"
+	"time"
 
 	"github.com/siglens/siglens/pkg/utils"
 	log "github.com/sirupsen/logrus"
@@ -22,22 +25,27 @@ func NewCertReloader(certPath string, privateKeyPath string) (*CertReloader, err
 	}
 
 	err := utils.OnFileChange([]string{certPath, privateKeyPath}, func() {
-		log.Infof("NewCertReloader: Reloading certificate")
+		log.Infof("NewCertReloader: Reloading certificate at %v with key at %v", certPath, privateKeyPath)
 		err := reloader.reload()
 		if err != nil {
-			log.Errorf("NewCertReloader: Error reloading certificate: %v", err)
+			log.Errorf("NewCertReloader: Error reloading certificate at %v with key at %v; err=%v",
+				certPath, privateKeyPath, err)
 		} else {
-			log.Infof("NewCertReloader: Successfully reloaded certificate")
+			log.Infof("NewCertReloader: Successfully reloaded certificate at %v with key at %v", certPath, privateKeyPath)
 		}
 	})
 	if err != nil {
-		log.Errorf("NewCertReloader: Error setting up certificate reloading: %v", err)
+		log.Errorf("NewCertReloader: Error setting up certificate reloading for certificate at %v with key at %v; err=%v",
+			certPath, privateKeyPath, err)
 		return nil, err
 	}
 
 	if err := reloader.reload(); err != nil {
-		log.Errorf("NewCertReloader: Error loading certificate: %v", err)
+		log.Errorf("NewCertReloader: Error loading certificate at %v with key at %v; err=%v",
+			certPath, privateKeyPath, err)
 		return nil, err
+	} else {
+		log.Infof("NewCertReloader: Successfully loaded certificate at %v with key at %v", certPath, privateKeyPath)
 	}
 
 	return reloader, nil
@@ -53,8 +61,26 @@ func (cr *CertReloader) GetCertificate(*tls.ClientHelloInfo) (*tls.Certificate, 
 func (cr *CertReloader) reload() error {
 	cert, err := tls.LoadX509KeyPair(cr.certPath, cr.keyPath)
 	if err != nil {
-		log.Errorf("reload: Error loading certificate: %v", err)
+		log.Errorf("reload: Error loading certificate at %v with key at %v; err=%v",
+			cr.certPath, cr.keyPath, err)
 		return err
+	}
+
+	// Check if the certificate time is valid
+	leaf, err := x509.ParseCertificate(cert.Certificate[0])
+	if err != nil {
+		log.Errorf("reload: Error parsing certificate: %v", err)
+		return err
+	}
+
+	now := time.Now()
+	if now.Before(leaf.NotBefore) {
+		return fmt.Errorf("reload: certificate at %v is not valid yet (not before: %v)",
+			cr.certPath, leaf.NotBefore)
+	}
+	if now.After(leaf.NotAfter) {
+		return fmt.Errorf("reload: certificate at %v has expired (not after: %v)",
+			cr.certPath, leaf.NotAfter)
 	}
 
 	cr.mu.Lock()
