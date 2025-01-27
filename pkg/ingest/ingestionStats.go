2c7994a7ff5b700b1ae6b7943d7cd8600283db18
Kunal Nawale <knawale@sigscalr.io>
2025-01-28 03:01:06 +0530

    fix lint
    Signed-off-by: Kunal Nawale <knawale@sigscalr.io>

--- pkg/ingest/ingestionStats.go
+++ pkg/ingest/ingestionStats.go
@@ -182,6 +182,10 @@ func setNumMetricNames() {
 	instrumentation.SetTotalMetricNames(int64(len(names)))
 }
 
+/*
+// TODO: disable calling series cardinality call every x minutes, since there is a bug which causes a panic
+// keep it disabled until the panic is fixed
+
 func setNumSeries() {
 	allPreviousTime := &dtu.MetricsTimeRange{
 		StartEpochSec: 0,
@@ -195,6 +199,7 @@ func setNumSeries() {
 
 	instrumentation.SetTotalTimeSeries(int64(numSeries))
 }
+*/
 
 func setNumKeysAndValues() {
 	allPreviousTime := &dtu.MetricsTimeRange{
