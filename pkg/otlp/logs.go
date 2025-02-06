c4c56ed2307ff7b35711d9bb833ec16a24ae31de
Andrew Hess <ahess@sigscalr.io>
2025-02-06 16:03:58 -0700

    Implement basic log ingestion

--- pkg/otlp/logs.go
+++ pkg/otlp/logs.go
@@ -18,11 +18,18 @@
 package otlp
 
 import (
+	"fmt"
+
+	"github.com/siglens/siglens/pkg/config"
+	"github.com/siglens/siglens/pkg/es/writer"
 	"github.com/siglens/siglens/pkg/grpc"
 	"github.com/siglens/siglens/pkg/hooks"
+	segwriter "github.com/siglens/siglens/pkg/segment/writer"
+	"github.com/siglens/siglens/pkg/utils"
 	log "github.com/sirupsen/logrus"
 	"github.com/valyala/fasthttp"
 	collogpb "go.opentelemetry.io/proto/otlp/collector/logs/v1"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -41,14 +48,136 @@ func ProcessLogIngest(ctx *fasthttp.RequestCtx, myid int64) {
 		return
 	}
 
-	_, err = unmarshalLogRequest(data)
+	request, err := unmarshalLogRequest(data)
 	if err != nil {
 		log.Errorf("ProcessTraceIngest: failed to unpack Data: %s with err %v", string(data), err)
 		setFailureResponse(ctx, fasthttp.StatusBadRequest, "Unable to unmarshal traces")
 		return
 	}
 
-	// err = writer.ProcessIndexRequestPle(now, indexName, shouldFlush, localIndexMap, myid, 0, idxToStreamIdCache, cnameCacheByteHashToStr, jsParsingStackbuf[:], pleArray)
+	type Scope struct {
+		Name                   string                 `json:"name"`
+		Version                string                 `json:"version"`
+		Attributes             map[string]interface{} `json:"attributes"`
+		DroppedAttributesCount int64                  `json:"droppedAttributesCount"`
+		SchemaUrl              string                 `json:"schemaUrl"`
+	}
+
+	type SingleRecord struct {
+		TimeUnixNano           uint64                 `json:"time_unix_nano"`
+		ObservedTimeUnixNano   uint64                 `json:"observed_time_unix_nano"`
+		SeverityNumber         int32                  `json:"severity_number"`
+		SeverityText           string                 `json:"severity_text"`
+		Body                   string                 `json:"body"`
+		Attributes             map[string]interface{} `json:"attributes"`
+		DroppedAttributesCount int64                  `json:"droppedAttributesCount"`
+		Flags                  uint32                 `json:"flags"`
+		TraceId                string                 `json:"traceId"`
+		SpanId                 string                 `json:"spanId"`
+	}
+
+	now := utils.GetCurrentTimeInMs()
+	timestampKey := config.GetTimeStampKey()
+	var jsParsingStackbuf [utils.UnescapeStackBufSize]byte
+	pleArray := make([]*segwriter.ParsedLogEvent, 0)
+
+	numFailedResources := 0
+
+resourceLoop:
+	for _, resourceLog := range request.ResourceLogs {
+		resource := resourceLog.Resource
+		sharedResourceInfo := make(map[string]interface{})
+		for _, attribute := range resource.Attributes {
+			key, value, err := extractKeyValue(attribute)
+			if err != nil {
+				log.Errorf("ProcessTraceIngest: failed to extract key value from attribute: %v", err)
+				numFailedResources++
+				continue resourceLoop
+			}
+			sharedResourceInfo[key] = value
+		}
+
+		sharedResourceInfo["resourceSchema"] = resourceLog.SchemaUrl
+		sharedResourceInfo["droppedAttributesCount"] = resource.DroppedAttributesCount
+
+		indexName := "otlp-default"
+		if value, ok := sharedResourceInfo["index"]; ok {
+			valueStr := fmt.Sprintf("%v", value)
+			if valueStr != "" {
+				indexName = valueStr
+			}
+		}
+
+		numFailedScopeLogs := 0
+
+	scopeLoop:
+		for _, scopeLog := range resourceLog.ScopeLogs {
+			scope := Scope{
+				Name:                   scopeLog.Scope.Name,
+				Version:                scopeLog.Scope.Version,
+				Attributes:             make(map[string]interface{}),
+				DroppedAttributesCount: int64(scopeLog.Scope.DroppedAttributesCount),
+				SchemaUrl:              scopeLog.SchemaUrl,
+			}
+
+			for _, attribute := range scopeLog.Scope.Attributes {
+				key, value, err := extractKeyValue(attribute)
+				if err != nil {
+					log.Errorf("ProcessTraceIngest: failed to extract key value from attribute: %v", err)
+					numFailedScopeLogs++
+					continue scopeLoop
+				}
+				scope.Attributes[key] = value
+			}
+
+			numFailedRecords := 0
+
+		recordLoop:
+			for _, logRecord := range scopeLog.LogRecords {
+				marshaler := protojson.MarshalOptions{
+					UseProtoNames:   false, // Use JSON names instead of proto names
+					EmitUnpopulated: true,  // Include zero values
+				}
+				jsonBytes, err := marshaler.Marshal(logRecord)
+				if err != nil {
+					log.Errorf("ProcessLogIngest: failed to marshal log record: %v", err)
+					numFailedRecords++
+					continue recordLoop
+				}
+
+				record := SingleRecord{}
+				for _, attribute := range logRecord.Attributes {
+					key, value, err := extractKeyValue(attribute)
+					if err != nil {
+						log.Errorf("ProcessLogIngest: failed to extract key value from attribute: %v", err)
+						numFailedRecords++
+						continue recordLoop
+					}
+
+					record.Attributes[key] = value
+				}
+
+				ple, err := segwriter.GetNewPLE(jsonBytes, now, indexName, &timestampKey, jsParsingStackbuf[:])
+				if err != nil {
+					log.Errorf("ProcessLogIngest: failed to get new PLE, jsonBytes: %v, err: %v", jsonBytes, err)
+					numFailedRecords++
+					continue resourceLoop
+				}
+				pleArray = append(pleArray, ple)
+			}
+		}
+	}
+
+	indexName := "andrew-test"
+	shouldFlush := false
+	localIndexMap := make(map[string]string)
+	idxToStreamIdCache := make(map[string]string)
+	cnameCacheByteHashToStr := make(map[uint64]string)
+	err = writer.ProcessIndexRequestPle(now, indexName, shouldFlush, localIndexMap, myid, 0, idxToStreamIdCache, cnameCacheByteHashToStr, jsParsingStackbuf[:], pleArray)
+	if err != nil {
+		log.Errorf("ProcessLogIngest: Failed to ingest logs, err: %v", err)
+		numFailedResources += len(pleArray) // TODO andrew
+	}
 	// if err != nil {
 	// 	log.Errorf("ProcessTraceIngest: Failed to ingest traces, err: %v", err)
 	// 	numFailedSpans += len(pleArray)
