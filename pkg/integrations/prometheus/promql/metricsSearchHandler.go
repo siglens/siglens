65b093470ca55fc8a1efc109bfea9a43b02f075d
Tom Constertina <tom@constertina.com>
2025-02-16 22:41:16 -0600

    Removed previous incorrect work

--- pkg/integrations/prometheus/promql/metricsSearchHandler.go
+++ pkg/integrations/prometheus/promql/metricsSearchHandler.go
@@ -1273,6 +1273,7 @@ func ConvertPqlToMetricsQuery(searchText string, startTime, endTime uint32, myid
 					mquery.Aggregator.AggregatorFunction = segutils.Avg
 				case "count":
 					mquery.Aggregator.AggregatorFunction = segutils.Count
+					mquery.GetAllLabels = true
 				case "sum":
 					mquery.Aggregator.AggregatorFunction = segutils.Sum
 				case "max":
@@ -1317,6 +1318,8 @@ func ConvertPqlToMetricsQuery(searchText string, startTime, endTime uint32, myid
 					mquery.Aggregator.AggregatorFunction = segutils.Avg
 				case "count":
 					mquery.Aggregator.AggregatorFunction = segutils.Count
+					mquery.GetAllLabels = true
+					mquery.TagsFilters = make([]*structs.TagsFilter, 0)
 				case "sum":
 					mquery.Aggregator.AggregatorFunction = segutils.Sum
 				case "max":
