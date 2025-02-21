e8c9c8b04d88ff8cc38127d71d5630b5e74f6deb
sonamgupta21 <sonam2001gupta@gmail.com>
2025-02-21 14:03:56 +0530

    Added Alert Cards

--- static/js/all-alerts.js
+++ static/js/all-alerts.js
@@ -34,12 +34,40 @@ let mapIndexToAlertType = new Map([
 
 $(document).ready(function () {
     $('.theme-btn').on('click', themePickerHandler);
-    getAllAlerts();
+    handlePageDisplay();
 
     $('#new-alert-rule').on('click', function () {
-        window.location.href = '../alert.html';
+        window.location.href = './all-alerts.html?page=create';
     });
+
+    $('#create-logs-alert').on('click', function () {
+        window.location.href = './alert.html?type=logs';
+    });
+
+    $('#create-metrics-alert').on('click', function () {
+        window.location.href = './alert.html?type=metrics';
+    });
+
+    lucide.createIcons();
 });
+
+function handlePageDisplay() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const page = urlParams.get('page');
+
+    const allAlertsPage = $('.all-alert-page');
+    const createAlertPage = $('.create-alert-page');
+
+    allAlertsPage.addClass('d-none');
+    createAlertPage.addClass('d-none');
+
+    if (page === 'create') {
+        createAlertPage.removeClass('d-none');
+    } else {
+        allAlertsPage.removeClass('d-none');
+    }
+    getAllAlerts();
+}
 
 //get all alerts
 function getAllAlerts() {
@@ -517,13 +545,24 @@ const alertGridOptions = {
 function displayAllAlerts(res) {
     if (alertGridDiv === null) {
         alertGridDiv = document.querySelector('#ag-grid');
-        //eslint-disable-next-line no-undef
         new agGrid.Grid(alertGridDiv, alertGridOptions);
     }
     alertGridOptions.api.setColumnDefs(alertColumnDefs);
     let newRow = new Map();
     let hasMutedAlerts = false;
+
+    // Add counters for logs and metrics alerts
+    let logsAlertCount = 0;
+    let metricsAlertCount = 0;
+
     $.each(res, function (key, value) {
+        // Count alerts by type
+        if (value.alert_type === 1) { // Logs
+            logsAlertCount++;
+        } else if (value.alert_type === 2) { // Metrics
+            metricsAlertCount++;
+        }
+
         newRow.set('rowId', key);
         newRow.set('alertId', value.alert_id);
         newRow.set('alertName', value.alert_name);
@@ -538,12 +577,16 @@ function displayAllAlerts(res) {
         newRow.set('alertType', mapIndexToAlertType.get(value.alert_type));
         newRow.set('silenceMinutes', value.silence_minutes);
         newRow.set('silenceEndTime', value.silence_end_time);
-        //eslint-disable-next-line no-undef
         const mutedFor = calculateMutedFor(value.silence_end_time);
         newRow.set('mutedFor', mutedFor);
         if (mutedFor) hasMutedAlerts = true;
         alertRowData = _.concat(alertRowData, Object.fromEntries(newRow));
     });
+
+    // Update the count displays in the UI
+    $('.logs-count').text(logsAlertCount + ' active');
+    $('.metrics-count').text(metricsAlertCount + ' active');
+
     alertGridOptions.api.setRowData(alertRowData);
     const mutedForColumn = alertGridOptions.columnApi.getColumn('mutedFor');
     if (mutedForColumn) {
