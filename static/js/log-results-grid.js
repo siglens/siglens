9688af9319b1531df4ea6d05983a28a5fcd511de
VaishRS0624 <vaishnavishinde9324@gmail.com>
2025-03-04 11:44:49 +0000

    Resolved merge conflicts

--- static/js/log-results-grid.js
+++ static/js/log-results-grid.js
@@ -17,7 +17,26 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-// initial columns
+/*
+ * Copyright (c) 2021-2024 SigScalr, Inc.
+ *
+ * This file is part of SigLens Observability Solution
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+let logsRowData = [];
 let logsColumnDefs = [
     {
         field: 'timestamp',
@@ -30,44 +49,16 @@ let logsColumnDefs = [
         field: 'logs',
         headerName: 'logs',
         minWidth: 1128,
-        cellRenderer: (params) => {
-
-            let logString = '';
-            let counter = 0;
-            if (updatedSelFieldList) {
-                selectedFieldsList = _.intersection(selectedFieldsList, availColNames);
-            } else {
-                selectedFieldsList = _.union(selectedFieldsList, availColNames);
-            }
-
-            if (selectedFieldsList.length != 0) {
-                availColNames.forEach((colName, _index) => {
-                    if (selectedFieldsList.includes(colName)) {
-                        $(`.toggle-${string2Hex(colName)}`).addClass('active');
-                    } else {
-                        $(`.toggle-${string2Hex(colName)}`).removeClass('active');
-                    }
-                });
-            }
-            _.forEach(params.data, (value, key) => {
-                let colSep = counter > 0 ? '<span class="col-sep"> | </span>' : '';
-                if (key != 'logs' && selectedFieldsList.includes(key)) {
-                    logString += `<span class="cname-hide-${string2Hex(key)}">${colSep}${key}=` + JSON.stringify(JSON.unflatten(value), null, 2) + `</span>`;
-                    counter++;
-                }
-            });
-            return logString;
-        },
     },
 ];
 
-// initial dataset
-let logsRowData = [];
 //eslint-disable-next-line no-unused-vars
 let allLiveTailColumns = [];
 //eslint-disable-next-line no-unused-vars
 let total_liveTail_searched = 0;
+
 // let the grid know which columns and what data to use
+//eslint-disable-next-line no-unused-vars
 const gridOptions = {
     columnDefs: logsColumnDefs,
     rowData: logsRowData,
@@ -86,25 +77,6 @@ const gridOptions = {
             sortAscending: '<i class="fa fa-sort-alpha-down"/>',
             sortDescending: '<i class="fa fa-sort-alpha-desc"/>',
         },
-        headerComponentParams: {
-            template: `<div class="ag-cell-label-container" role="presentation">
-                  <span ref="eMenu" class="ag-header-icon ag-header-cell-menu-button"></span>
-                  <span ref="eFilterButton" class="ag-header-icon ag-header-cell-filter-button"></span>
-                  <div ref="eLabel" class="ag-header-cell-label" role="presentation">
-                    <div style="display: flex; align-items: center; justify-content: space-between; gap: 3px; width: 100%;">
-                        <div style="display: flex; align-items: center; gap: 3px;">
-                            <span ref="eText" class="ag-header-cell-text" role="columnheader"></span>
-                            <span ref="eSortOrder" class="ag-header-icon ag-sort-order"></span>
-                            <span ref="eSortAsc" class="ag-header-icon ag-sort-ascending-icon"></span>
-                            <span ref="eSortDesc" class="ag-header-icon ag-sort-descending-icon"></span>
-                            <span ref="eSortNone" class="ag-header-icon ag-sort-none-icon"></span>
-                            <span ref="eFilter" class="ag-header-icon ag-filter-icon"></span>
-                        </div>
-                        <i onclick="hideColumnHandler(event, true)" class="fa fa-close close-icon"></i>
-                    </div>
-                  </div>
-                </div>`,
-        },
     },
     icons: {
         sortAscending: '<i class="fa fa-sort-alpha-down"/>',
@@ -117,28 +89,7 @@ const gridOptions = {
     animateRows: false,
     suppressColumnVirtualisation: false,
     suppressRowVirtualisation: false,
-    onGridReady: function (_params) {
-        const eGridDiv = document.querySelector('#LogResultsGrid');
-        const style = document.createElement('style');
-        style.textContent = `
-            .close-icon {
-                cursor: pointer;
-                color: #888;
-                margin-left: 5px;
-                display: none;
-            }
-
-            .ag-header-cell:not([col-id="timestamp"]):not([col-id="logs"]):hover .close-icon {
-                display: inline-block;
-            }
-        `;
-        eGridDiv.appendChild(style);
-
-    },
-
 };
-
-console.log(logsRowData);
 
 //eslint-disable-next-line no-unused-vars
 const myCellRenderer = (params) => {
@@ -150,26 +101,6 @@ const myCellRenderer = (params) => {
     }
     return value;
 };
-//eslint-disable-next-line no-unused-vars
-function updateColumns() {
-    // Always show timestamp
-    gridOptions.columnApi.setColumnVisible('timestamp', true);
-    let isAnyColActive = false;
-    availColNames.forEach((colName, _index) => {
-        if ($(`.toggle-${string2Hex(colName)}`).hasClass('active')) {
-            isAnyColActive = true;
-            gridOptions.columnApi.setColumnVisible(colName, true);
-        } else {
-            gridOptions.columnApi.setColumnVisible(colName, false);
-        }
-    });
-
-    if (isAnyColActive) {
-        // Always hide logs column if we have some fields selected
-        gridOptions.columnApi.setColumnVisible('logs', false);
-    }
-    gridOptions.api.sizeColumnsToFit();
-}
 
 JSON.unflatten = function (data) {
     if (Object(data) !== data || Array.isArray(data)) return data;
