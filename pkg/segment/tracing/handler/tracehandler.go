0bdeef485734293f5fe3c61fabfbf658118339ca
Vasudev Raut <vasudevraut9912@gmail.com>
2025-03-07 13:22:01 +0000

    Fix: Handles both signed and unsigned integers for epoch conversion.

--- pkg/segment/tracing/handler/tracehandler.go
+++ pkg/segment/tracing/handler/tracehandler.go
@@ -793,7 +793,7 @@ func convertEpochToString(value interface{}) string {
 	switch v := value.(type) {
 	case string:
 		return v
-	case int, int8, int16, int32, int64:
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
 		return fmt.Sprintf("%d", v)
 	case float32, float64:
 		return fmt.Sprintf("%f", v)
