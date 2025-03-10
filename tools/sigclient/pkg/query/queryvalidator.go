912303c48fce630a7b93f93a0d9bd526a13d8197
Andrew Hess <ahess@sigscalr.io>
2025-03-10 16:16:58 -0600

    Add basic validator framework

---
+++ tools/sigclient/pkg/query/queryvalidator.go
@@ -0,0 +1,34 @@
+// Copyright (c) 2021-2025 SigScalr, Inc.
+//
+// This file is part of SigLens Observability Solution
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package query
+
+type queryValidator interface {
+	HandleLog(map[string]interface{}) error
+	GetExpectedResult() string          // JSON response.
+	GetQuery() (string, uint64, uint64) // Query, start epoch, end epoch.
+}
+
+type basicValidator struct {
+	startEpoch uint64
+	endEpoch   uint64
+	query      string
+}
+
+func (b *basicValidator) GetQuery() (string, uint64, uint64) {
+	return b.query, b.startEpoch, b.endEpoch
+}
