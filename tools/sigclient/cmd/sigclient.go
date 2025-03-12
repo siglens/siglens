2fbdba88e4f7ac7b7153266955fb644c414ef541
Andrew Hess <ahess@sigscalr.io>
2025-03-12 16:56:51 -0600

    Fix queries

--- tools/sigclient/cmd/sigclient.go
+++ tools/sigclient/cmd/sigclient.go
@@ -551,8 +551,8 @@ var longevityCmd = &cobra.Command{
 		dataGeneratorConfig := utils.InitGeneratorDataConfig(maxCols, true, minCols, uniqueCols)
 
 		templates := []*query.QueryTemplate{
-			query.NewQueryTemplate(unwrap(query.NewFilterQueryValidator("city", "Boston", 10, 0, 0)), 300, 10),
-			query.NewQueryTemplate(unwrap(query.NewFilterQueryValidator("city", "Boston", 10, 0, 0)), 1, 1),
+			query.NewQueryTemplate(unwrap(query.NewFilterQueryValidator("city_c1", "Boston", 10, 0, 0)), 300, 10),
+			query.NewQueryTemplate(unwrap(query.NewFilterQueryValidator("city_c1", "Boston", 10, 0, 0)), 1, 1),
 		}
 		maxConcurrentQueries := int32(1)
 		queryManager := query.NewQueryManager(templates, maxConcurrentQueries, queryUrl)
