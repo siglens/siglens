7dc4f7bbe0a5d28edb078da1969d61b96eecb88f
VaishRS0624 <vaishnavishinde9324@gmail.com>
2025-03-18 06:12:03 +0000

    Removed extra code

--- static/js/log-search.js
+++ static/js/log-search.js
@@ -28,10 +28,6 @@ $(document).ready(async () => {
     fieldssidebarRenderer.init();
     window.fieldssidebarRenderer = fieldssidebarRenderer;
 
-    // Apply the initial state to the DOM but not to the URL unless search params exist
-    const { isFieldsSidebarHidden } = fieldssidebarRenderer.getState();
-    applyFieldsSidebarState(isFieldsSidebarHidden);
-
     // Call the function for each tooltip
     createTooltip('#add-index', 'Add New Index');
     createTooltip('#date-picker-btn', 'Pick the Time Window');
