6c784cbbb9d3aa27e1921241f22d1bb6d7e3247e
Andrew Hess <ahess@sigscalr.io>
2025-03-20 15:53:16 -0600

    Add match-all stats queries to longevity test

--- tools/sigclient/cmd/sigclient.go
+++ tools/sigclient/cmd/sigclient.go
@@ -575,6 +575,8 @@ var longevityCmd = &cobra.Command{
 			query.NewQueryTemplate(unwrap(query.NewCountQueryValidator("app_version_c1", "1.2.3", 0, 0)), 6*3600, 100),
 			query.NewQueryTemplate(unwrap(query.NewCountQueryValidator("state_c1", "Texas", 0, 0)), 1, 1),
 			query.NewQueryTemplate(unwrap(query.NewCountQueryValidator("state_c1", "Texas", 0, 0)), 300, 10),
+			query.NewQueryTemplate(unwrap(query.NewCountQueryValidator("*", "*", 0, 0)), 1, 1),
+			query.NewQueryTemplate(unwrap(query.NewCountQueryValidator("*", "*", 0, 0)), 300, 10),
 		}
 		maxConcurrentQueries := int32(1)
 		queryManager := query.NewQueryManager(templates, maxConcurrentQueries, queryUrl)
