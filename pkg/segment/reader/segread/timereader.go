363de38420cb5fcc84f1c9d070d17af4298be5aa
Andrew Hess <ahess@sigscalr.io>
2025-03-31 10:52:57 -0600

    Fix infinite loop in error path

--- pkg/segment/reader/segread/timereader.go
+++ pkg/segment/reader/segread/timereader.go
@@ -371,7 +371,6 @@ func ReadAllTimestampsForBlock(blks map[uint16]*structs.BlockMetadataHolder, seg
 
 	retVal := make(map[uint16][]uint64)
 	var retLock sync.Mutex
-	minIdx := 0
 	allReadJob := make(chan *timeBlockRequest)
 	var readerWG sync.WaitGroup
 	for i := int64(0); i < parallelism; i++ {
@@ -379,12 +378,12 @@ func ReadAllTimestampsForBlock(blks map[uint16]*structs.BlockMetadataHolder, seg
 		go processTimeBlocks(allReadJob, &readerWG, retVal, &retLock)
 	}
 
-	for minIdx < len(allBlocks) {
+	for minIdx, maxIdx := 0, 0; minIdx < len(allBlocks); minIdx = maxIdx + 1 {
 		minBlkNum := allBlocks[minIdx]
 		lastBlkNum := minBlkNum
 		firstBlkOff := blks[minBlkNum].ColumnBlockOffset[tsKey]
 		blkLen := blks[minBlkNum].ColumnBlockLen[tsKey]
-		maxIdx := minIdx
+		maxIdx = minIdx
 		for {
 			nextIdx := maxIdx + 1
 			if nextIdx >= len(allBlocks) {
@@ -414,7 +413,6 @@ func ReadAllTimestampsForBlock(blks map[uint16]*structs.BlockMetadataHolder, seg
 			numRecs := blockSummaries[currBlk].RecCount
 			allReadJob <- &timeBlockRequest{tsRec: rawBlock, blkNum: currBlk, numRecs: numRecs}
 		}
-		minIdx = maxIdx + 1
 	}
 	close(allReadJob)
 	readerWG.Wait()
