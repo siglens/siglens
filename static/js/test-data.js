4cbe8f9af51f3a317df255f4c18fb421bb3036a8
VaishRS0624 <vaishnavishinde9324@gmail.com>
2025-04-04 12:08:46 +0000

    Made some changes

--- static/js/test-data.js
+++ static/js/test-data.js
@@ -35,7 +35,7 @@ $(document).ready(async function () {
             xhrFields: { withCredentials: true }
         });
         if (config.IngestUrl) {
-            baseUrl = config.IngestUrl;
+            baseUrl = config.IngestUrl.replace(/^http:/, 'https:');
         }
         {{ if .TestDataSendData }}
             {{ .TestDataSendData }}
@@ -44,6 +44,14 @@ $(document).ready(async function () {
         {{ end }}
     } catch (err) {
         console.log(err);
+    }
+
+    function setCodeBlockContainerBackground() {
+        const preElement = $('.code-block-container pre.language-yaml');
+        if (preElement.length) {
+            const preBackgroundColor = preElement.css('background-color');
+            $('.code-block-container').css('background-color', preBackgroundColor);
+        }
     }
 
     $('#custom-chart-tab').on('click', '.tab-li', function () {
@@ -56,14 +64,42 @@ $(document).ready(async function () {
             {{ .IngestDataCmd }}
         {{ end }}
 
-        var curlCommand = 'curl -X POST "' + baseUrl + '/elastic/_bulk" \\\n' +
-            '-H \'Content-Type: application/json\' \\\n' +
-            ingestCmd +
-            '-d \'{ "index" : { "_index" : "test" } }\n' +
-            '{ "name" : "john", "age":"23" }\'';
-        $('#verify-command').text(curlCommand);
+        // Hide test data button by default
+        $('#test-data-btn').hide();
+        $('#data-ingestion').show();
+
 
         switch (selectedLogSource) {
+            case 'OpenTelemetry':
+                $('#platform-input').val(selectedLogSource);
+                $('#setup-instructions-link').attr('href', 'https://www.siglens.com/siglens-docs/log-ingestion/open-telemetry/');
+                // Example OTLP command for logs
+                var curlCommand = 'curl -X POST "' + baseUrl + '/v1/logs" \\\n' +
+                    '-H \'Content-Type: application/json\' \\\n' +
+                    '-H \'Accept: application/json\' \\\n' +
+                    ingestCmd +
+                    '-d \'{\n' +
+                    '  "resourceLogs": [{\n' +
+                    '    "resource": {\n' +
+                    '      "attributes": [{\n' +
+                    '        "key": "service.name",\n' +
+                    '        "value": { "stringValue": "test-service" }\n' +
+                    '      }]\n' +
+                    '    },\n' +
+                    '    "scopeLogs": [{\n' +
+                    '      "logRecords": [{\n' +
+                    '        "timeUnixNano": "1234567890000000000",\n' +
+                    '        "severityText": "INFO",\n' +
+                    '        "body": { "stringValue": "This is a test log" }\n' +
+                    '      }]\n' +
+                    '    }]\n' +
+                    '  }]\n' +
+                    '}\'';
+                $('#verify-command').text(curlCommand);
+                Prism.highlightElement($('#verify-command')[0]);
+                updateCodeDisplay();
+                setCodeBlockContainerBackground();
+                break;
             case 'Vector':
             case 'Logstash':
             case 'Fluentd':
@@ -71,10 +107,30 @@ $(document).ready(async function () {
             case 'Promtail':
                 $('#platform-input').val(selectedLogSource);
                 $('#setup-instructions-link').attr('href', 'https://www.siglens.com/siglens-docs/log-ingestion/' + selectedLogSource.toLowerCase());
+                // Keep existing elastic bulk command
+                var curlCommand = 'curl -X POST "' + baseUrl + '/elastic/_bulk" \\\n' +
+                    '-H \'Content-Type: application/json\' \\\n' +
+                    ingestCmd +
+                    '-d \'{ "index" : { "_index" : "test" } }\n' +
+                    '{ "name" : "john", "age":"23" }\'';
+                $('#verify-command').text(curlCommand);
+                Prism.highlightElement($('#verify-command')[0]);
+                updateCodeDisplay();
+                setCodeBlockContainerBackground();
                 break;
             case 'Elastic Bulk':
                 $('#platform-input').val(selectedLogSource);
                 $('#setup-instructions-link').attr('href', 'https://www.siglens.com/siglens-docs/migration/elasticsearch/fluentd');
+                // Keep existing elastic bulk command
+                var curlCommand = 'curl -X POST "' + baseUrl + '/elastic/_bulk" \\\n' +
+                    '-H \'Content-Type: application/json\' \\\n' +
+                    ingestCmd +
+                    '-d \'{ "index" : { "_index" : "test" } }\n' +
+                    '{ "name" : "john", "age":"23" }\'';
+                $('#verify-command').text(curlCommand);
+                Prism.highlightElement($('#verify-command')[0]);
+                updateCodeDisplay();
+                setCodeBlockContainerBackground();
                 break;
             case 'Splunk HEC':
                 $('#platform-input').val(selectedLogSource);
@@ -84,8 +140,12 @@ $(document).ready(async function () {
                     ingestCmd +
                     '-d \'{ "index": "test", "name": "john", "age": "23"}\'';
                 $('#verify-command').text(curlCommand);
+                Prism.highlightElement($('#verify-command')[0]);
+                updateCodeDisplay();
+                setCodeBlockContainerBackground();
                 break;
             case 'Send Test Data':
+                $('#data-ingestion').hide();
                 $('#test-data-btn').show();
                 break;
             default:
@@ -93,6 +153,7 @@ $(document).ready(async function () {
         }
     });
 
+    // Update the initial tab selection to be OpenTelemetry
     $('.custom-chart-tab ul li:first a').click();
 
     //Copy Handler
@@ -101,23 +162,92 @@ $(document).ready(async function () {
         $(this).after(copyIcon);
     });
 
-    $('.copy-icon').on('click', function (_event) {
-        var copyIcon = $(this);
-        var inputOrTextarea = copyIcon.prev('.copyable');
-        var inputValue = inputOrTextarea.val();
+    // Replace the copy button handler in your JavaScript
+    $('.copy-btn').on('click', function (_event) {
+        var copyBtn = $(this);
+        var textarea = copyBtn.siblings('#verify-command');
+        var textToCopy = textarea.val();
 
         var tempInput = document.createElement("textarea");
-        tempInput.value = inputValue;
+        tempInput.value = textToCopy;
         document.body.appendChild(tempInput);
         tempInput.select();
         document.execCommand("copy");
         document.body.removeChild(tempInput);
 
-        copyIcon.addClass('success');
+        copyBtn.addClass('success');
+
+        var originalSvg = copyBtn.html();
+
+        copyBtn.html('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>');
+
         setTimeout(function () {
-            copyIcon.removeClass('success');
+            copyBtn.removeClass('success');
+            copyBtn.html(originalSvg);
         }, 1000);
     });
+
+    // Function to check if the code overflows and show/hide the expand button
+    function checkCodeOverflow() {
+        $('.code-wrapper').each(function() {
+            var codeWrapper = $(this);
+            var preElement = codeWrapper.find('pre');
+            var expandBtn = codeWrapper.parent().find('.expand-btn');
+
+            // Reset to collapsed state if not already expanded
+            if (!codeWrapper.hasClass('expanded')) {
+                expandBtn.find('span').text('Expand code');
+                expandBtn.find('.expand-icon').css('transform', 'rotate(0deg)');
+            }
+
+            // Force a reflow to ensure accurate measurements
+            preElement.width();
+
+            var isVerticalOverflow = preElement[0].scrollHeight > codeWrapper.height();
+            var isHorizontalOverflow = preElement[0].scrollWidth > preElement.width();
+
+
+            if (isVerticalOverflow || isHorizontalOverflow) {
+                expandBtn.css('display', 'flex');
+            } else {
+                expandBtn.css('display', 'none');
+            }
+        });
+    }
+
+    $('.expand-btn').on('click', function() {
+        var expandBtn = $(this);
+        var codeWrapper = expandBtn.parent().find('.code-wrapper');
+        var isExpanded = codeWrapper.hasClass('expanded');
+
+        if (isExpanded) {
+            // Collapse the code block
+            codeWrapper.removeClass('expanded');
+            expandBtn.find('span').text('Expand code');
+            expandBtn.find('.expand-icon').css('transform', 'rotate(0deg)');
+        } else {
+            // Expand the code block
+            codeWrapper.addClass('expanded');
+            expandBtn.find('span').text('Collapse code');
+            expandBtn.find('.expand-icon').css('transform', 'rotate(180deg)');
+        }
+    });
+
+    // Initial check for code overflow
+    setTimeout(checkCodeOverflow, 100);
+
+    // Add window resize handler
+    $(window).on('resize', function() {
+        checkCodeOverflow();
+    });
+
+    // Make sure to call this after any content changes
+    function updateCodeDisplay() {
+        if (window.Prism) {
+            Prism.highlightAll();
+        }
+        setTimeout(checkCodeOverflow, 100);
+    }
 
     {{ .Button1Function }}
 })
