82667e8b6258a524e06c2ed9550a13d0ad15d708
sonamgupta21 <sonam2001gupta@gmail.com>
2025-04-08 12:22:46 +0530

    UI: Fix chart point hover effects

--- static/js/usage-stats.js
+++ static/js/usage-stats.js
@@ -296,7 +296,7 @@ function renderStackedChart(data, gridLineColor, tickColor) {
                     borderWidth: 2,
                     tension: 0.3,
                     pointRadius: 3,
-                    pointHoverRadius: 5,
+                    pointHoverRadius: 6,
                     fill: false,
                 },
                 {
@@ -307,7 +307,7 @@ function renderStackedChart(data, gridLineColor, tickColor) {
                     borderWidth: 2,
                     tension: 0.3,
                     pointRadius: 3,
-                    pointHoverRadius: 5,
+                    pointHoverRadius: 6,
                     fill: false,
                 },
             ],
@@ -315,6 +315,10 @@ function renderStackedChart(data, gridLineColor, tickColor) {
         options: {
             responsive: true,
             maintainAspectRatio: false,
+            interaction: {
+                mode: 'index',
+                intersect: false,
+            },
             plugins: {
                 tooltip: {
                     mode: 'index',
