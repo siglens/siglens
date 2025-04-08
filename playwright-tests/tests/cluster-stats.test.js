fba7c71088a613f024aea68e4a8918bb01f73447
Vaishnavi Shinde <123016841+VaishRS0624@users.noreply.github.com>
2025-04-08 19:21:26 +0530

    removal of some UI for cluster stats (#2470)
    * removel of some UI for cluster stats
    
    * Removed date picker and fixed playwright
    
    * Addressed the comments

--- playwright-tests/tests/cluster-stats.test.js
+++ playwright-tests/tests/cluster-stats.test.js
@@ -9,30 +9,11 @@ test('Cluster Stats Page Test', async ({ page }) => {
     await expect(page.locator('#app-content-area')).toBeVisible();
     await expect(page.locator('#cstats-app-footer')).toBeVisible();
 
-    // Check the time picker
-    const datePickerBtn = page.locator('#date-picker-btn');
-    await expect(datePickerBtn).toBeVisible();
-    await datePickerBtn.click();
-    await expect(page.locator('.daterangepicker')).toBeVisible();
-
-    // Check for the presence of time range options
-    const timeRanges = ['1 Hr', '3 Hrs', '6 Hrs', '12 Hrs', '24 Hrs', '7 Days', '30 Days', '90 Days', '180 Days', '1 Year'];
-    for (const range of timeRanges) {
-        await expect(page.locator(`.range-item:text("${range}")`)).toBeVisible();
-    }
 
     // Check for the presence of stats sections
     await expect(page.locator('text=Logs Stats')).toBeVisible();
     await expect(page.locator('text=Metrics Stats')).toBeVisible();
     await expect(page.locator('text=Traces Stats')).toBeVisible();
-
-    // Check for the presence of charts
-    await expect(page.locator('#EventCountChart-logs')).toBeVisible();
-    await expect(page.locator('#bytesCountChart-logs')).toBeVisible();
-    await expect(page.locator('#EventCountChart-metrics')).toBeVisible();
-    await expect(page.locator('#bytesCountChart-metrics')).toBeVisible();
-    await expect(page.locator('#EventCountChart-trace')).toBeVisible();
-    await expect(page.locator('#bytesCountChart-trace')).toBeVisible();
 
     // Check for the presence of data tables
     await expect(page.locator('#index-data-table')).toBeVisible();
