8f2b52d7644c94525094f81cc467a93ac7cb948f
sonamgupta21 <sonam2001gupta@gmail.com>
2025-04-23 17:42:29 +0530

    Fix Lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -232,6 +232,8 @@ export default [
                 tippy: true,
                 getGraphGridColors: true,
                 updateChartColorsBasedOnTheme: true,
+                hljs: true,
+                marked: true
             },
         },
         rules: {
