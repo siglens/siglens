0210eec66b3f06fca3f564c376491525001044c5
Andrew Hess <ahess@sigscalr.io>
2025-05-01 16:31:38 -0600

    Add util for getting top N without doing full sort

---
+++ pkg/utils/heap.go
@@ -0,0 +1,116 @@
+// Copyright (c) 2021-2025 SigScalr, Inc.
+//
+// # This file is part of SigLens Observability Solution
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package utils
+
+import "container/heap"
+
+type HeapItem[T any] struct {
+	Value T
+	index int // Position in the heap, maintained by heap operations
+}
+
+type lessFunc[T any] func(a, b T) bool
+
+// Heap implements heap.Interface for a generic collection of items.
+type Heap[T any] struct {
+	items []*HeapItem[T]
+	less  lessFunc[T]
+}
+
+func NewHeap[T any](less lessFunc[T]) *Heap[T] {
+	if less == nil {
+		return nil
+	}
+
+	return &Heap[T]{
+		items: []*HeapItem[T]{},
+		less:  less,
+	}
+}
+
+func (h *Heap[T]) Len() int {
+	return len(h.items)
+}
+
+func (h *Heap[T]) Less(i, j int) bool {
+	return h.less(h.items[i].Value, h.items[j].Value)
+}
+
+func (h *Heap[T]) Swap(i, j int) {
+	h.items[i], h.items[j] = h.items[j], h.items[i]
+	h.items[i].index = i
+	h.items[j].index = j
+}
+
+func (h *Heap[T]) Push(x any) {
+	n := len(h.items)
+	item := x.(*HeapItem[T])
+	item.index = n
+	h.items = append(h.items, item)
+}
+
+// Remove and return the last item.
+func (h *Heap[T]) Pop() any {
+	old := h.items
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	h.items = old[0 : n-1]
+	return item
+}
+
+func (h *Heap[T]) PushValue(v T) *HeapItem[T] {
+	item := &HeapItem[T]{Value: v}
+	heap.Push(h, item)
+	return item
+}
+
+func (h *Heap[T]) PopValue() T {
+	return heap.Pop(h).(*HeapItem[T]).Value
+}
+
+func GetTopN[T any](N int, items []T, less lessFunc[T]) []T {
+	// Create min-heap (smaller items at root)
+	h := NewHeap(func(a, b T) bool {
+		return less(b, a) // Reverse the order for min-heap
+	})
+
+	// Process all items
+	for _, item := range items {
+		h.PushValue(item)
+
+		// If we have more than N items, remove the smallest
+		if h.Len() > N {
+			h.PopValue()
+		}
+	}
+
+	// Extract results in reverse order (smallest to largest)
+	result := make([]T, 0, h.Len())
+	for h.Len() > 0 {
+		result = append(result, h.PopValue())
+	}
+
+	// Reverse to get descending order
+	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+		result[i], result[j] = result[j], result[i]
+	}
+
+	return result
+}
---
+++ pkg/utils/heap_test.go
@@ -0,0 +1,109 @@
+// Copyright (c) 2021-2025 SigScalr, Inc.
+//
+// # This file is part of SigLens Observability Solution
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package utils
+
+import (
+	"container/heap"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ImplementsHeap(t *testing.T) {
+	var _ heap.Interface = NewHeap(func(a, b int) bool { return a < b })
+}
+
+func TestHeapBasicOperations(t *testing.T) {
+	// Max heap for integers
+	h := NewHeap(func(a, b int) bool { return a > b })
+
+	// Test empty heap
+	if h.Len() != 0 {
+		t.Errorf("Expected empty heap, got %d elements", h.Len())
+	}
+
+	// Add elements
+	heap.Init(h)
+	heap.Push(h, &HeapItem[int]{Value: 5})
+	heap.Push(h, &HeapItem[int]{Value: 3})
+	heap.Push(h, &HeapItem[int]{Value: 7})
+
+	// Check size
+	if h.Len() != 3 {
+		t.Errorf("Expected 3 elements, got %d", h.Len())
+	}
+
+	// Verify max element
+	max := heap.Pop(h).(*HeapItem[int])
+	if max.Value != 7 {
+		t.Errorf("Expected max value 7, got %d", max.Value)
+	}
+
+	// Verify remaining elements
+	secondMax := heap.Pop(h).(*HeapItem[int])
+	if secondMax.Value != 5 {
+		t.Errorf("Expected second max value 5, got %d", secondMax.Value)
+	}
+
+	thirdMax := heap.Pop(h).(*HeapItem[int])
+	if thirdMax.Value != 3 {
+		t.Errorf("Expected third max value 3, got %d", thirdMax.Value)
+	}
+
+	// Verify empty again
+	if h.Len() != 0 {
+		t.Errorf("Expected empty heap after popping all elements, got %d", h.Len())
+	}
+}
+
+func TestHeapConvenienceMethods(t *testing.T) {
+	// Min heap for strings
+	h := NewHeap(func(a, b string) bool { return a < b })
+	heap.Init(h)
+
+	// Use convenience methods
+	h.PushValue("banana")
+	h.PushValue("apple")
+	h.PushValue("cherry")
+
+	if h.Len() != 3 {
+		t.Errorf("Expected 3 elements, got %d", h.Len())
+	}
+
+	// Verify order with PopValue
+	if val := h.PopValue(); val != "apple" {
+		t.Errorf("Expected 'apple', got '%s'", val)
+	}
+	if val := h.PopValue(); val != "banana" {
+		t.Errorf("Expected 'banana', got '%s'", val)
+	}
+	if val := h.PopValue(); val != "cherry" {
+		t.Errorf("Expected 'cherry', got '%s'", val)
+	}
+}
+
+func TestTopN(t *testing.T) {
+	data := []int{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9}
+
+	top5 := GetTopN(5, data, func(a, b int) bool {
+		return a > b
+	})
+
+	assert.Equal(t, 5, len(top5))
+	assert.Equal(t, []int{9, 9, 9, 8, 7}, top5)
+}
