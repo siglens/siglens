36463c6c9b05dcf1244903e9997aea6b822e2b28
Sonam Gupta <71771131+sonamgupta21@users.noreply.github.com>
2026-02-28 11:32:40 +0530

    Revert PR 1527 (#2957)

--- pkg/ast/spl/tests/splParser_test.go
+++ pkg/ast/spl/tests/splParser_test.go
@@ -11459,61 +11459,6 @@ func Test_RemoveRedundantSearches(t *testing.T) {
 	assert.Equal(t, expectedAggregator, aggregator)
 }
 
-func Test_Append_1(t *testing.T) {
-	query := `* | append [ search foo=bar ]`
-	res, err := spl.Parse("", []byte(query))
-	assert.Nil(t, err)
-	filterNode := res.(ast.QueryStruct).SearchFilter
-	assert.NotNil(t, filterNode)
-
-	astNode, aggregator, _, err := pipesearch.ParseQuery(string(query), 0, "Splunk QL")
-	assert.Nil(t, err)
-	assert.NotNil(t, astNode)
-	assert.NotNil(t, aggregator)
-	assert.Equal(t, structs.OutputTransformType, aggregator.PipeCommandType)
-	assert.NotNil(t, aggregator.OutputTransforms)
-	assert.NotNil(t, aggregator.OutputTransforms.LetColumns)
-
-	assert.NotNil(t, aggregator.OutputTransforms.LetColumns.AppendRequest)
-	assert.NotNil(t, aggregator.OutputTransforms.LetColumns.AppendRequest.Subsearch)
-
-	subSearch, _ := aggregator.OutputTransforms.LetColumns.AppendRequest.Subsearch.(*ast.Node)
-	assert.Equal(t, "foo", subSearch.Comparison.Field)
-	assert.Equal(t, "=", subSearch.Comparison.Op)
-	assert.Equal(t, "bar", strings.Trim(subSearch.Comparison.Values.(string), `"`))
-
-	assert.Equal(t, 60, aggregator.OutputTransforms.LetColumns.AppendRequest.MaxTime)
-	assert.Equal(t, 50000, aggregator.OutputTransforms.LetColumns.AppendRequest.MaxOut)
-
-}
-
-func Test_Append_2(t *testing.T) {
-	query := `* | append maxtime=30 maxout=10000 [ search foo=bar ] `
-	res, err := spl.Parse("", []byte(query))
-	assert.Nil(t, err)
-	filterNode := res.(ast.QueryStruct).SearchFilter
-	assert.NotNil(t, filterNode)
-
-	astNode, aggregator, _, err := pipesearch.ParseQuery(string(query), 0, "Splunk QL")
-	assert.Nil(t, err)
-	assert.NotNil(t, astNode)
-	assert.NotNil(t, aggregator)
-	assert.Equal(t, structs.OutputTransformType, aggregator.PipeCommandType)
-	assert.NotNil(t, aggregator.OutputTransforms)
-	assert.NotNil(t, aggregator.OutputTransforms.LetColumns)
-	assert.NotNil(t, aggregator.OutputTransforms.LetColumns.AppendRequest)
-	assert.NotNil(t, aggregator.OutputTransforms.LetColumns.AppendRequest.Subsearch)
-
-	assert.Equal(t, 30, aggregator.OutputTransforms.LetColumns.AppendRequest.MaxTime)
-	assert.Equal(t, 10000, aggregator.OutputTransforms.LetColumns.AppendRequest.MaxOut)
-
-	subSearch, _ := aggregator.OutputTransforms.LetColumns.AppendRequest.Subsearch.(*ast.Node)
-	assert.Equal(t, "foo", subSearch.Comparison.Field)
-	assert.Equal(t, "=", subSearch.Comparison.Op)
-	assert.Equal(t, "bar", strings.Trim(subSearch.Comparison.Values.(string), `"`))
-
-}
-
 func Test_Index(t *testing.T) {
 	query := `_index=main foo=bar`
 	res, err := spl.Parse("", []byte(query))
