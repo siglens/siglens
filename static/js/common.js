6e2a113d8eb085ec53a5d44fbc13940578a2c06b
sonamgupta21 <sonam2001gupta@gmail.com>
2026-03-11 23:42:35 +0530

    Fix

--- static/js/common.js
+++ static/js/common.js
@@ -566,13 +566,6 @@ async function runMetricsQuery(data, panelId, currentPanel, _queryRes) {
                 addVisualizationContainer(formulaData.formulas[0].formula, chartData, formulaString, panelId);
             }
         }
-        // if (currentPanel && currentPanel.style) {
-        //     toggleLineOptions(currentPanel.style.display);
-        //     chartType = currentPanel.style.display;
-        //     toggleChartType(chartType);
-        //     updateChartTheme(currentPanel.style.color);
-        //     updateLineCharts(currentPanel.style.lineStyle, currentPanel.style.lineStroke);
-        // }
         $(`#panel${panelId} .panel-body #panel-loading`).hide();
         allResultsDisplayed--;
         if (allResultsDisplayed <= 0 || panelId === -1) {
